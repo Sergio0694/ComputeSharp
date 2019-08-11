f3e66111a4d58b67c615dbddf21b267fa0398c04
Sergio Pedri <sergio0694@live.com>
2019-08-11 02:54:56 +0200

    Sample program switched to lambda expression

--- src/DirectX12ComputeShaderSample/Program.cs
+++ src/DirectX12ComputeShaderSample/Program.cs
@@ -11,63 +11,54 @@ using PipelineState = DirectX12GameEngine.Graphics.PipelineState;
 
 namespace DirectX12ComputeShaderSample
 {
-    public class MyComputeShader
-    {
-        [ConstantBufferResource] public uint Width;
-
-        [ShaderResource] public RWBufferResource<float> Data;
-
-        [ShaderMethod]
-        [Shader("compute")]
-        [NumThreads(5, 5, 1)]
-        public void CSMain([SystemDispatchThreadIdSemantic] UInt3 id)
-        {
-            Data[id.X + id.Y * Width] *= 2;
-        }
-    }
-
     public class Program
     {
         private static void Main()
         {
-            // Generate computer shader
-
-            MyComputeShader myComputeShader = new MyComputeShader();
-            ShaderGenerator shaderGenerator = new ShaderGenerator(myComputeShader);
-            ShaderGenerationResult result = shaderGenerator.GenerateShader();
-
-            byte[] shaderBytecode = ShaderCompiler.CompileShader(result.ShaderSource, SharpDX.D3DCompiler.ShaderVersion.ComputeShader, result.ComputeShader);
-
-            // Create graphics device and pipeline state
-
+            // Create the graphics device
             using GraphicsDevice device = new GraphicsDevice(true);
 
-            SharpDX.Direct3D12.RootParameter[] rootParameters = new SharpDX.Direct3D12.RootParameter[]
-            {
-                new SharpDX.Direct3D12.RootParameter(SharpDX.Direct3D12.ShaderVisibility.All,
-                    new SharpDX.Direct3D12.RootConstants(0, 0, 1)),
-                new SharpDX.Direct3D12.RootParameter(SharpDX.Direct3D12.ShaderVisibility.All,
-                    new SharpDX.Direct3D12.DescriptorRange(SharpDX.Direct3D12.DescriptorRangeType.UnorderedAccessView, 1, 0))
-            };
-
-            var rootSignatureDescription = new SharpDX.Direct3D12.RootSignatureDescription(SharpDX.Direct3D12.RootSignatureFlags.None, rootParameters);
-            var rootSignature = device.CreateRootSignature(rootSignatureDescription);
-
-            PipelineState pipelineState = new PipelineState(device, rootSignature, shaderBytecode);
-
-            // Create graphics buffer
-
+            // Create the graphics buffer
             int width = 10;
             int height = 10;
-
             float[] array = new float[width * height];
-
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = i + 1;
             }
 
             using Buffer<float> gpuBuffer = Buffer.UnorderedAccess.New(device, array.AsSpan());
+
+            // Variables for closure
+            var data = new RWBufferResource<float>();
+            uint Width = (uint)width;
+
+            // Shader body
+            Action<UInt3> action = id =>
+            {
+                data[id.X + id.Y * Width] *= 2;
+            };
+
+            // Generate computer shader
+            ShaderGenerator shaderGenerator = new ShaderGenerator(action);
+            ShaderGenerationResult result = shaderGenerator.GenerateShaderForLambda();
+
+            byte[] shaderBytecode = ShaderCompiler.CompileShader(result.ShaderSource, SharpDX.D3DCompiler.ShaderVersion.ComputeShader, result.ComputeShader);
+
+            // Create the pipeline state
+
+            RootParameter[] rootParameters =
+            {
+                new RootParameter(ShaderVisibility.All, new RootConstants(0, 0, 1)),
+                new RootParameter(ShaderVisibility.All, new DescriptorRange(DescriptorRangeType.UnorderedAccessView, 1, 0))
+            };
+
+            var rootSignatureDescription = new RootSignatureDescription(RootSignatureFlags.None, rootParameters);
+            var rootSignature = device.CreateRootSignature(rootSignatureDescription);
+
+            PipelineState pipelineState = new PipelineState(device, rootSignature, shaderBytecode);
+
+            
 
             // Execute computer shader
 
