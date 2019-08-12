4c533ad2f63a6a8517c213b054a212d2750dc978
Sergio Pedri <sergio0694@live.com>
2019-08-12 15:15:37 +0200

    Code refactoring to the Gpu class, moved to extension class

---
+++ src/ComputeSharp/Extensions/GraphicsDeviceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ComputeSharp.Graphics;
+using ComputeSharp.Shaders;
+
+namespace ComputeSharp
+{
+    /// <summary>
+    /// A <see langword="class"/> that contains extension methods for the <see cref="GraphicsDevice"/> type, used to run compute shaders
+    /// </summary>
+    public static class GraphicsDeviceExtensions
+    {
+        /// <summary>
+        /// The default number of threads in a thead froup
+        /// </summary>
+        public const int DefaultThreadGroupCount = 64;
+
+        /// <summary>
+        /// Compiles and runs the input shader on a target <see cref="GraphicsDevice"/> instance, with the specified parameters
+        /// </summary>
+        /// <param name="device">The <see cref="GraphicsDevice"/> to use to run the shader</param>
+        /// <param name="x">The number of threads to run on the X axis</param>
+        /// <param name="action">The input <see cref="Action{T}"/> representing the compute shader to run</param>
+        public static void For(this GraphicsDevice device, int x, Action<ThreadIds> action)
+        {
+            ShaderRunner.Run(device, (x, 1), (1, 1), action);
+        }
+    }
+}
--- src/ComputeSharp/Gpu.cs
+++ src/ComputeSharp/Gpu.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Linq;
-using ComputeSharp.Graphics;
-using ComputeSharp.Shaders;
-using ComputeSharp.Shaders.Renderer;
-using ComputeSharp.Shaders.Renderer.Models;
-using ComputeSharp.Shaders.Translation;
-using SharpDX.Direct3D12;
-using CommandList = ComputeSharp.Graphics.CommandList;
-using PipelineState = ComputeSharp.Graphics.PipelineState;
-using ShaderBytecode = SharpDX.Direct3D12.ShaderBytecode;
+﻿using ComputeSharp.Graphics;
 
 namespace ComputeSharp
 {
@@ -23,48 +13,5 @@ namespace ComputeSharp
         /// Gets the default <see cref="GraphicsDevice"/> instance for the current machine
         /// </summary>
         public static GraphicsDevice Default => _Default ??= new GraphicsDevice(true);
-    }
-
-    public static class GraphicsDeviceExtensions
-    {
-        public static void For(this GraphicsDevice device, int n, Action<ThreadIds> action)
-        {
-            // Load the input shader
-            ShaderLoader shaderLoader = ShaderLoader.Load(action);
-
-            // Render the loaded shader
-            ShaderInfo shaderInfo = new ShaderInfo
-            {
-                FieldsList = shaderLoader.FieldsInfo,
-                ThreadsX = n,
-                ThreadsY = 1,
-                ThreadsZ = 1,
-                ThreadsIdsVariableName = shaderLoader.ThreadsIdsVariableName,
-                ShaderBody = shaderLoader.MethodBody
-            };
-            string shaderSource = ShaderRenderer.Instance.Render(shaderInfo);
-
-            // Compile the loaded shader to HLSL bytecode
-            ShaderBytecode shaderBytecode = ShaderCompiler.Instance.CompileShader(shaderSource);
-
-            // Create the root signature for the pipeline and the pipeline state
-            RootSignatureDescription rootSignatureDescription = new RootSignatureDescription(RootSignatureFlags.None, shaderLoader.RootParameters);
-            RootSignature rootSignature = device.CreateRootSignature(rootSignatureDescription);
-            PipelineState pipelineState = new PipelineState(device, rootSignature, shaderBytecode);
-
-            // Create the commands list
-            using CommandList commandList = new CommandList(device, CommandListType.Compute);
-            commandList.SetPipelineState(pipelineState);
-
-            // Load the captured buffers
-            foreach (var buffer in shaderLoader.ReadWriteBuffers.Select((r, i) => (Resource: r, Index: i)))
-            {
-                commandList.SetComputeRootDescriptorTable(buffer.Index, buffer.Resource);
-            }
-
-            // Dispatch and wait for completion
-            commandList.Dispatch(1, 1, 1);
-            commandList.Flush(true);
-        }
     }
 }
