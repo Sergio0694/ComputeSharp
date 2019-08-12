27898b0878ff03178c1a2576433322ee46fc6da5
Sergio Pedri <sergio0694@live.com>
2019-08-13 01:36:24 +0200

    Added unit test project

---
+++ tests/ComputeSharp.NetCore.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputeSharp.NetCore.Sample
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
