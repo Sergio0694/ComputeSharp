9b69b65ad752409b0cd4063dd29acbd0563b802c
Sergio Pedri <sergio0694@live.com>
2019-08-12 14:22:29 +0200

    Moved Gpu class to the main ComputeSharp project

--- src/ComputeSharp/Gpu.cs
+++ src/ComputeSharp/Gpu.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using ComputeSharp;
 using ComputeSharp.Graphics;
 using ComputeSharp.Shaders;
 using ComputeSharp.Shaders.Renderer;
@@ -10,12 +9,18 @@ using CommandList = ComputeSharp.Graphics.CommandList;
 using PipelineState = ComputeSharp.Graphics.PipelineState;
 using ShaderBytecode = SharpDX.Direct3D12.ShaderBytecode;
 
-namespace DirectX12ComputeShaderSample
+namespace ComputeSharp
 {
+    /// <summary>
+    /// A <see langword="class"/> that acts as an entry-point for all the library APIs, exposing the available GPU devices
+    /// </summary>
     public static class Gpu
     {
         private static GraphicsDevice _Default;
 
+        /// <summary>
+        /// Gets the default <see cref="GraphicsDevice"/> instance for the current machine
+        /// </summary>
         public static GraphicsDevice Default => _Default ??= new GraphicsDevice(true);
     }
 
