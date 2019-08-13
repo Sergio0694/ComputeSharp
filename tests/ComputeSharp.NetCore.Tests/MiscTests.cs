279d7c372f378b664ba42123782404b07a7f7e4b
Sergio Pedri <sergio0694@live.com>
2019-08-13 13:10:57 +0200

    Added GPU initialization tests

---
+++ tests/ComputeSharp.NetCore.Tests/MiscTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputeSharp.NetCore.Tests
+{
+    [TestClass]
+    [TestCategory("Misc")]
+    public class MiscTests
+    {
+        [TestMethod]
+        public void CheckIsSupportedAndGetDefaultDevice()
+        {
+            if (Gpu.IsSupported) Assert.IsNotNull(Gpu.Default);
+            else Assert.ThrowsException<NotSupportedException>(() => Gpu.Default);
+        }
+    }
+}
