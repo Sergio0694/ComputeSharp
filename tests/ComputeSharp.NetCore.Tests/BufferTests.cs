d5a80657d8f2e1f69666f0ce2ea8e0e65da3cf66
Sergio Pedri <sergio0694@live.com>
2019-08-14 14:44:48 +0200

    Added tests for new reflected buffer API

--- tests/ComputeSharp.NetCore.Tests/BufferTests.cs
+++ tests/ComputeSharp.NetCore.Tests/BufferTests.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Linq;
 using ComputeSharp.Graphics.Buffers;
+using ComputeSharp.Graphics.Buffers.Abstract;
+using ComputeSharp.Graphics.Buffers.Extensions;
 using ComputeSharp.NetCore.Tests.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -10,6 +12,30 @@ namespace ComputeSharp.NetCore.Tests
     [TestCategory("Buffers")]
     public class BufferTests
     {
+        [TestMethod]
+        public void ReadWriteBufferGetSetDataFromSingleValueInt()
+        {
+            object value = 77;
+
+            using ReadOnlyBuffer<int> buffer = (ReadOnlyBuffer<int>)Gpu.Default.AllocateReadOnlyBufferFromReflectedSingleValue(value);
+            int[] result = buffer.GetData();
+
+            Assert.IsTrue(result.Length == 1);
+            Assert.IsTrue((int)value == result[0]);
+        }
+
+        [TestMethod]
+        public void ReadWriteBufferGetSetDataFromSingleValueFloat()
+        {
+            object value = 3.14f;
+
+            using ReadOnlyBuffer<float> buffer = (ReadOnlyBuffer<float>)Gpu.Default.AllocateReadOnlyBufferFromReflectedSingleValue(value); 
+            float[] result = buffer.GetData();
+
+            Assert.IsTrue(result.Length == 1);
+            Assert.IsTrue(MathF.Abs(result[0] - (float)value) < 0.0001f);
+        }
+
         [TestMethod]
         public void ReadWriteBufferGetSetDataFromArray()
         {
