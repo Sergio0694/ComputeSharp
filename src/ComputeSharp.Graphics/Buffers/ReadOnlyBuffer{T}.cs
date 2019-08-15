673633a48d88d6822615bc43aaffe754d2839439
Sergio Pedri <sergio0694@live.com>
2019-08-15 15:55:58 +0200

    Added ReadOnlyBuffer<T> type

---
+++ src/ComputeSharp.Graphics/Buffers/ReadOnlyBuffer{T}.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ComputeSharp.Graphics.Buffers.Abstract;
+using ComputeSharp.Graphics.Buffers.Enums;
+
+namespace ComputeSharp.Graphics.Buffers
+{
+    /// <summary>
+    /// A <see langword="class"/> representing a typed readonly buffer stored on GPU memory
+    /// </summary>
+    /// <typeparam name="T">The type of items stored on the buffer</typeparam>
+    public sealed class ReadOnlyBuffer<T> : HlslStructuredBuffer<T> where T : unmanaged
+    {
+        /// <summary>
+        /// Creates a new <see cref="ReadOnlyBuffer{T}"/> instance with the specified parameters
+        /// </summary>
+        /// <param name="device">The <see cref="GraphicsDevice"/> associated with the current instance</param>
+        /// <param name="size">The number of items to store in the current buffer</param>
+        internal ReadOnlyBuffer(GraphicsDevice device, int size) : base(device, size, BufferType.ReadOnly) { }
+
+        /// <summary>
+        /// Gets or sets a single <typeparamref name="T"/> value from the current read write buffer
+        /// </summary>
+        /// <param name="i">The index of the value to get or set</param>
+        /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else</remarks>
+        public T this[uint i] => throw new InvalidOperationException("The indexer APIs can only be used from a compute shader");
+    }
+}
