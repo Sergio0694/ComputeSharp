ab9d5944dcbec87640f9f08cf8800f5cbe41c580
Sergio Pedri <sergio0694@live.com>
2019-08-17 22:33:19 +0200

    Updated benchmark code

--- samples/ComputeSharp.Benchmark/Program.cs
+++ samples/ComputeSharp.Benchmark/Program.cs
@@ -7,12 +7,17 @@ namespace ComputeSharp.Benchmark
     {
         static void Main()
         {
-            var benchmark = new FullyConnectedLayerBenchmark();
-            benchmark.Gpu(); // Warmup
+            using var benchmark = new FullyConnectedLayerBenchmark();
 
+            Console.WriteLine("======== WARMUP ========");
+            benchmark.GpuWithNoTemporaryBuffers(); // Warmup
+
+            Console.WriteLine();
+            Console.WriteLine("======== GPU with temporary buffer ========");
             Stopwatch timer = Stopwatch.StartNew();
             TimeSpan cpuTime = TimeSpan.Zero, gpuTime = TimeSpan.Zero;
 
+            // GPU with temporary buffers
             for (int i = 0; i < 10; i++)
             {
                 benchmark.Cpu();
@@ -20,12 +25,38 @@ namespace ComputeSharp.Benchmark
                 Console.WriteLine($"CPU: {timer.Elapsed:g}");
                 timer.Restart();
 
-                benchmark.Gpu();
+                benchmark.GpuWithTemporaryBuffers();
                 gpuTime += timer.Elapsed;
                 Console.WriteLine($"GPU: {timer.Elapsed:g}");
                 timer.Restart();
             }
 
+            Console.WriteLine();
+            Console.WriteLine("======== AVERAGE ========");
+            Console.WriteLine($"CPU: {cpuTime / 10:g}");
+            Console.WriteLine($"GPU: {gpuTime / 10:g}");
+            Console.WriteLine("=========================");
+            Console.WriteLine();
+
+            Console.WriteLine("======== GPU with no temporary buffer ========");
+            timer = Stopwatch.StartNew();
+            cpuTime = gpuTime = TimeSpan.Zero;
+
+            // GPU with no temporary buffers
+            for (int i = 0; i < 10; i++)
+            {
+                benchmark.Cpu();
+                cpuTime += timer.Elapsed;
+                Console.WriteLine($"CPU: {timer.Elapsed:g}");
+                timer.Restart();
+
+                benchmark.GpuWithNoTemporaryBuffers();
+                gpuTime += timer.Elapsed;
+                Console.WriteLine($"GPU: {timer.Elapsed:g}");
+                timer.Restart();
+            }
+
+            Console.WriteLine();
             Console.WriteLine("======== AVERAGE ========");
             Console.WriteLine($"CPU: {cpuTime / 10:g}");
             Console.WriteLine($"GPU: {gpuTime / 10:g}");
