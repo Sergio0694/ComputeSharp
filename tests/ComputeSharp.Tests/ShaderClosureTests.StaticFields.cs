240bbe777eeb7bc02f80c5cffda8de2eab365634
Sergio Pedri <sergio0694@live.com>
2019-08-18 12:25:01 +0200

    Added test for static buffer

--- tests/ComputeSharp.Tests/ShaderClosureTests.StaticFields.cs
+++ tests/ComputeSharp.Tests/ShaderClosureTests.StaticFields.cs
@@ -29,6 +29,11 @@ namespace ComputeSharp.Tests
         /// A <see langword="static"/> <see langword="Vector2"/> field
         /// </summary>
         public static Vector2 StaticVector2 = new Vector2(3.14f, 7.77f);
+
+        /// <summary>
+        /// A <see langword="static"/> <see cref="ReadOnlyBuffer{T}"/> field
+        /// </summary>
+        public static ReadOnlyBuffer<float> StaticBuffer = Gpu.Default.AllocateReadOnlyBuffer(new[] { 3.14f, 7.77f });
     }
 
     public partial class ShaderClosureTests
@@ -111,6 +116,26 @@ namespace ComputeSharp.Tests
 
             Assert.IsTrue(MathF.Abs(result[0] - StaticFieldsContainer.StaticVector2.X) < 0.0001f);
             Assert.IsTrue(MathF.Abs(result[1] - StaticFieldsContainer.StaticVector2.Y) < 0.0001f);
+        }
+
+        [TestMethod]
+        public void StaticReadOnlyBufferAssignToBuffer()
+        {
+            using ReadWriteBuffer<float> buffer = Gpu.Default.AllocateReadWriteBuffer<float>(2);
+
+            Action<ThreadIds> action = id =>
+            {
+                if (id.X == 0) buffer[0] = StaticFieldsContainer.StaticBuffer[0];
+                else buffer[1] = StaticFieldsContainer.StaticBuffer[1];
+            };
+
+            Gpu.Default.For(2, action);
+
+            float[] result = buffer.GetData();
+            float[] expected = StaticFieldsContainer.StaticBuffer.GetData();
+
+            Assert.IsTrue(MathF.Abs(result[0] - expected[0]) < 0.0001f);
+            Assert.IsTrue(MathF.Abs(result[1] - expected[1]) < 0.0001f);
         }
     }
 }
