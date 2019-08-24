d7599ef181a899d8274405c6d1e3f93bc2168ae7
Sergio Pedri <sergio0694@live.com>
2019-08-24 23:57:25 +0200

    Added tests for the new intrinsics

---
+++ tests/ComputeSharp.Tests/IntrinsicTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ComputeSharp.Graphics.Buffers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputeSharp.Tests
+{
+    [TestClass]
+    [TestCategory("IntrinsicTests")]
+    public class IntrinsicTests
+    {
+        [TestMethod]
+        public void LocalScalarAssignToBuffer()
+        {
+            float angle = 80;
+            using ReadWriteBuffer<float> buffer = Gpu.Default.AllocateReadWriteBuffer<float>(4);
+
+            Action<ThreadIds> action = id =>
+            {
+                buffer[0] = Hlsl.Sin(angle);
+                buffer[1] = Hlsl.Cos(angle);
+                Hlsl.SinCos(angle, out buffer[2], out buffer[3]);
+            };
+
+            Gpu.Default.For(1, action);
+
+            float[] result = buffer.GetData();
+
+            Assert.IsTrue(MathF.Abs(result[0] - result[2]) < 0.0001f);
+            Assert.IsTrue(MathF.Abs(result[1] - result[3]) < 0.0001f);
+        }
+    }
+}
