93bf73f259a3275d2711981e09048e697a3bc6f8
Sergio Pedri <sergio0694@live.com>
2019-08-25 23:23:34 +0200

    Added Double2, Double3 and Double4 structs

---
+++ src/ComputeSharp.Shaders/Primitives/Double/Double2.cs
@@ -0,0 +1,123 @@
+﻿using System.Runtime.InteropServices;
+using ComputeSharp.Graphics.Exceptions;
+
+namespace ComputeSharp.Shaders.Primitives.Double
+{
+    /// <summary>
+    /// A <see langword="struct"/> that maps the double2 HLSL type
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Size = sizeof(double) * 2)]
+    public struct Double2
+    {
+        /// <summary>
+        /// Creates a new <see cref="Double2"/> instance with the specified parameters
+        /// </summary>
+        /// <param name="x">The value to assign to the first vector component</param>
+        /// <param name="y">The value to assign to the second vector component</param>
+        public Double2(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// Gets or sets the value of the first vector component
+        /// </summary>
+        public double X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of the second vector component
+        /// </summary>
+        public double Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of the first color component
+        /// </summary>
+        public double R
+        {
+            get => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(R)}");
+            set => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(R)}");
+        }
+
+        /// <summary>
+        /// Gets or sets the value of the second color component
+        /// </summary>
+        public double G
+        {
+            get => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(G)}");
+            set => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(G)}");
+        }
+
+        /// <summary>
+        /// Gets or sets a specific component in the current <see cref="Double2"/> instance
+        /// </summary>
+        /// <param name="i">The index of the component to access</param>
+        public double this[int i]
+        {
+            get => throw new InvalidExecutionContextException($"{nameof(Double2)}[int]");
+            set => throw new InvalidExecutionContextException($"{nameof(Double2)}[int]");
+        }
+
+        /// <summary>
+        /// Gets an <see cref="Double2"/> value with the <see cref="X"/> value for all components
+        /// </summary>
+        public Double2 XX => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(XX)}");
+
+        /// <summary>
+        /// Gets or sets an <see cref="Double2"/> value with the <see cref="X"/> and <see cref="Y"/> values
+        /// </summary>
+        public Double2 XY
+        {
+            get => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(XY)}");
+            set => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(XY)}");
+        }
+
+        /// <summary>
+        /// Gets an <see cref="Double2"/> value with the <see cref="Y"/> and <see cref="X"/> values
+        /// </summary>
+        public Double2 YX
+        {
+            get => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(YX)}");
+            set => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(YX)}");
+        }
+
+        /// <summary>
+        /// Gets an <see cref="Double2"/> value with the <see cref="Y"/> value for all components
+        /// </summary>
+        public Double2 YY => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(YY)}");
+
+        /// <summary>
+        /// Gets an <see cref="Double2"/> value with the <see cref="R"/> value for all components
+        /// </summary>
+        public Double2 RR => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(RR)}");
+
+        /// <summary>
+        /// Gets or sets an <see cref="Double2"/> value with the <see cref="R"/> and <see cref="G"/> values
+        /// </summary>
+        public Double2 RG
+        {
+            get => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(RG)}");
+            set => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(RG)}");
+        }
+
+        /// <summary>
+        /// Gets an <see cref="Double2"/> value with the <see cref="G"/> and <see cref="R"/> values
+        /// </summary>
+        public Double2 GR
+        {
+            get => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(GR)}");
+            set => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(GR)}");
+        }
+
+        /// <summary>
+        /// Gets an <see cref="Double2"/> value with the <see cref="G"/> value for all components
+        /// </summary>
+        public Double2 GG => throw new InvalidExecutionContextException($"{nameof(Double2)}.{nameof(GG)}");
+
+        /// <summary>
+        /// Creates a new <see cref="Double2"/> value with the same value for all its components
+        /// </summary>
+        /// <param name="x">The value to use for the components of the new <see cref="Double2"/> instance</param>
+        public static implicit operator Double2(double x) => new Double2(x, x);
+    }
+}
