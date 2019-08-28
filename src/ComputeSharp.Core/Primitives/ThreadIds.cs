399218f525b43d458f9db57b83c4fb720b57c85a
Sergio Pedri <sergio0694@live.com>
2019-08-29 00:31:35 +0200

    Code tweaks to the ThreadIds struct

--- src/ComputeSharp.Core/Primitives/ThreadIds.cs
+++ src/ComputeSharp.Core/Primitives/ThreadIds.cs
@@ -1,4 +1,6 @@
-﻿namespace ComputeSharp
+﻿using ComputeSharp.Exceptions;
+
+namespace ComputeSharp
 {
     /// <summary>
     /// A <see langword="struct"/> that indicates the ids of a given GPU thread running a compute shader
@@ -6,18 +8,18 @@
     public readonly struct ThreadIds
     {
         /// <summary>
-        /// The X id of the current thread
+        /// Gets the X id of the current thread
         /// </summary>
-        public readonly int X;
+        public int X => throw new InvalidExecutionContextException($"{nameof(ThreadIds)}.{nameof(X)}");
 
         /// <summary>
-        /// The Y id of the current thread
+        /// Gets the Y id of the current thread
         /// </summary>
-        public readonly int Y;
+        public int Y => throw new InvalidExecutionContextException($"{nameof(ThreadIds)}.{nameof(Y)}");
 
         /// <summary>
-        /// The Z id of the current thread
+        /// Gets the Z id of the current thread
         /// </summary>
-        public readonly int Z;
+        public int Z => throw new InvalidExecutionContextException($"{nameof(ThreadIds)}.{nameof(Z)}");
     }
 }
