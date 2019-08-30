6e52f289b686d7aedc210466a9f984be22c910fe
Sergio Pedri <sergio0694@live.com>
2019-08-30 22:37:41 +0200

    Fixed some XML comments

--- src/ComputeSharp.Graphics/Buffers/Enums/BufferType.cs
+++ src/ComputeSharp.Graphics/Buffers/Enums/BufferType.cs
@@ -6,27 +6,27 @@
     internal enum BufferType
     {
         /// <summary>
-        /// A constant buffer, with items aligned to 16 bytes, used for individual values or very small arrays, mapped to <see cref="SharpDX.Direct3D12.HeapType.Upload"/>
+        /// A constant buffer, with items aligned to 16 bytes, used for individual values or very small arrays, mapped to <see cref="Vortice.DirectX.Direct3D12.HeapType.Upload"/>
         /// </summary>
         Constant,
 
         /// <summary>
-        /// A readonly buffer, that can store arbitrary arrays of values, mapped to <see cref="SharpDX.Direct3D12.HeapType.Default"/>
+        /// A readonly buffer, that can store arbitrary arrays of values, mapped to <see cref="Vortice.DirectX.Direct3D12.HeapType.Default"/>
         /// </summary>
         ReadOnly,
 
         /// <summary>
-        /// A read write buffer, that can store arbitrary arrays with both read and write access, mapped to <see cref="SharpDX.Direct3D12.HeapType.Default"/>
+        /// A read write buffer, that can store arbitrary arrays with both read and write access, mapped to <see cref="Vortice.DirectX.Direct3D12.HeapType.Default"/>
         /// </summary>
         ReadWrite,
 
         /// <summary>
-        /// A readback buffer, used as temporary buffer to read data back from read write buffers, mapped to <see cref="SharpDX.Direct3D12.HeapType.Readback"/>
+        /// A readback buffer, used as temporary buffer to read data back from read write buffers, mapped to <see cref="Vortice.DirectX.Direct3D12.HeapType.Readback"/>
         /// </summary>
         ReadBack,
 
         /// <summary>
-        /// A transfer buffer, used as a temporary buffer to set data to buffers in the default heap, mapped to <see cref="SharpDX.Direct3D12.HeapType.Upload"/>
+        /// A transfer buffer, used as a temporary buffer to set data to buffers in the default heap, mapped to <see cref="Vortice.DirectX.Direct3D12.HeapType.Upload"/>
         /// </summary>
         Transfer
     }
