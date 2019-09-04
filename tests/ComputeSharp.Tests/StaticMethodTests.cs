a3bc07e88d7d131ff258eeec81b7eccb02e1b61f
Sergio Pedri <sergio0694@live.com>
2019-09-04 15:40:13 +0200

    Added a test for a static method that takes an HLSL vector type

--- tests/ComputeSharp.Tests/StaticMethodTests.cs
+++ tests/ComputeSharp.Tests/StaticMethodTests.cs
@@ -14,6 +14,9 @@ namespace ComputeSharp.Tests
 
         [Pure]
         public static Float4 Range(float x) => new Float4(x, x + 1, x + 2, x + 3);
+
+        [Pure]
+        public static int Sum(Float4 x) => (int)(x.X + x.Y + x.Z + x.W);
     }
 
     [TestClass]
@@ -45,6 +48,18 @@ namespace ComputeSharp.Tests
             Assert.IsTrue(MathF.Abs(result[0].Y - 4) < 0.0001f);
             Assert.IsTrue(MathF.Abs(result[0].Z - 5) < 0.0001f);
             Assert.IsTrue(MathF.Abs(result[0].W - 6) < 0.0001f);
+        }
+
+        [TestMethod]
+        public void Float4ToIntFunc()
+        {
+            using ReadWriteBuffer<int> buffer = Gpu.Default.AllocateReadWriteBuffer<int>(1);
+
+            Gpu.Default.For(1, id => buffer[0] = StaticMethodsContainer.Sum(new Float4(1, 2, 3, 14)));
+
+            int[] result = buffer.GetData();
+
+            Assert.IsTrue(result[0] == 20);
         }
     }
 }
