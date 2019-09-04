22d281eee80cd72de35e9e6cbba98883e2c4e91c
Sergio Pedri <sergio0694@live.com>
2019-09-04 11:58:07 +0200

    Added new template models for static functions

---
+++ src/ComputeSharp.Shaders/Renderer/Models/Functions/FunctionInfo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using ComputeSharp.Shaders.Extensions;
+
+namespace ComputeSharp.Shaders.Renderer.Models.Functions
+{
+    /// <summary>
+    /// A <see langword="class"/> that contains info on a shader function
+    /// </summary>
+    internal class FunctionInfo
+    {
+        /// <summary>
+        /// Gets the return type of the current function in the C# source
+        /// </summary>
+        public string FunctionCsharpReturnType { get; }
+
+        /// <summary>
+        /// Gets the fullname of the current function in the C# source
+        /// </summary>
+        public string FunctionFullName { get; }
+
+        /// <summary>
+        /// Gets the parameters of the function in the C# source
+        /// </summary>
+        public string FunctionCsharpParameters { get; }
+
+        /// <summary>
+        /// Gets the return type of the current function
+        /// </summary>
+        public string ReturnType { get; }
+
+        /// <summary>
+        /// Gets the name of the current function
+        /// </summary>
+        public string FunctionName { get; }
+
+        /// <summary>
+        /// Gets the body of the current function
+        /// </summary>
+        public string FunctionBody { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="FunctionInfo"/> instance with the specified parameters
+        /// </summary>
+        /// <param name="functionCsharpType">The concrete return type for the function</param>
+        /// <param name="functionFullname">The fullname of the function in the C# source</param>
+        /// <param name="functionCsharpParameters">The parameters of the function in the C# source</param>
+        /// <param name="returnType">The return type of the current function</param>
+        /// <param name="functionName">The name of the current function</param>
+        /// <param name="functionBody">The current function</param>
+        public FunctionInfo(
+            Type functionCsharpType,
+            string functionFullname,
+            string functionCsharpParameters,
+            string returnType,
+            string functionName,
+            string functionBody)
+        {
+            FunctionCsharpReturnType = functionCsharpType.ToFriendlyString();
+            FunctionFullName = functionFullname;
+            FunctionCsharpParameters = functionCsharpParameters;
+            ReturnType = returnType;
+            FunctionName = functionName;
+            FunctionBody = functionBody;
+        }
+    }
+}
---
+++ src/ComputeSharp.Shaders/Renderer/Models/Functions/ParameterInfo.cs
@@ -0,0 +1,36 @@
+﻿namespace ComputeSharp.Shaders.Renderer.Models.Functions
+{
+    /// <summary>
+    /// A <see langword="class"/> that contains info on a function parameter
+    /// </summary>
+    internal class ParameterInfo
+    {
+        /// <summary>
+        /// Gets the type of the current parameter
+        /// </summary>
+        public string ParameterType { get; }
+
+        /// <summary>
+        /// Gets the name to use for the current parameter
+        /// </summary>
+        public string ParameterName { get; }
+
+        /// <summary>
+        /// Gets whether or not the current parameter is the last one for the parent function
+        /// </summary>
+        public bool IsLastParameter { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="ParameterInfo"/> instance with the specified parameters
+        /// </summary>
+        /// <param name="parameterType">The type of the current parameter</param>
+        /// <param name="parameterName">The name of the current parameter</param>
+        /// <param name="last">Indicates whether or not the current parameter is the last one</param>
+        public ParameterInfo(string parameterType, string parameterName, bool last)
+        {
+            ParameterType = parameterType;
+            ParameterName = parameterName;
+            IsLastParameter = last;
+        }
+    }
+}
