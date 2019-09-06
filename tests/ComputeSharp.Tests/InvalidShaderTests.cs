10e35c4991ad882a7422233fe17cda2ee9511964
Sergio Pedri <sergio0694@live.com>
2019-09-06 21:53:44 +0200

    Added test for new exception

--- tests/ComputeSharp.Tests/InvalidShaderTests.cs
+++ tests/ComputeSharp.Tests/InvalidShaderTests.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using ComputeSharp.Exceptions;
+using ComputeSharp.Graphics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ComputeSharp.Tests
@@ -13,6 +16,18 @@ namespace ComputeSharp.Tests
             Action<ThreadIds> action = id => { };
 
             Assert.ThrowsException<InvalidOperationException>(() => Gpu.Default.For(10, action));
+        }
+
+        [TestMethod]
+        public void DeviceMismatchException()
+        {
+            ReadWriteBuffer<int> buffer = Gpu.Default.AllocateReadWriteBuffer<int>(1);
+
+            GraphicsDevice device = Gpu.EnumerateDevices().First();
+
+            Action<ThreadIds> action = id => buffer[0] = 1;
+
+            Assert.ThrowsException<GraphicsDeviceMismatchException>(() => device.For(1, action));
         }
     }
 }
