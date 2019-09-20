562a4674745286d33e1c5e42e6ad8be58d113e30
Sergio Pedri <sergio0694@live.com>
2019-09-20 03:13:18 +0200

    Improved tests for scalar types in buffers

--- tests/ComputeSharp.Tests/ShaderClosureTests.cs
+++ tests/ComputeSharp.Tests/ShaderClosureTests.cs
@@ -10,19 +10,35 @@ namespace ComputeSharp.Tests
     [TestCategory("ShaderClosure")]
     public class ShaderClosureTests
     {
+        private static void LocalScalarAssignToBuffer<T>(T value, Func<T, T, bool> equals) where T : unmanaged
+        {
+            using ReadWriteBuffer<T> buffer = Gpu.Default.AllocateReadWriteBuffer<T>(4);
+
+            T zero = default;
+
+            Action<ThreadIds> action = id =>
+            {
+                if (id.X % 2 == 0) buffer[id.X] = value;
+                else buffer[id.X] = zero;
+            };
+
+            Gpu.Default.For(4, action);
+
+            T[] result = buffer.GetData();
+
+            Assert.IsTrue(equals(result[0], value));
+            Assert.IsFalse(equals(result[1], value));
+            Assert.IsTrue(equals(result[2], value));
+            Assert.IsFalse(equals(result[3], value));
+        }
+
         [TestMethod]
         public void LocalScalarAssignToBuffer()
         {
-            float value = 10;
-            using ReadWriteBuffer<float> buffer = Gpu.Default.AllocateReadWriteBuffer<float>(1);
-
-            Action<ThreadIds> action = id => buffer[0] = value;
-
-            Gpu.Default.For(1, action);
-
-            float[] result = buffer.GetData();
-
-            Assert.IsTrue(MathF.Abs(result[0] - value) < 0.0001f);
+            LocalScalarAssignToBuffer(10, (a, b) => a == b);
+            LocalScalarAssignToBuffer(3.14f, (a, b) => MathF.Abs(a - b) < 0.001f);
+            LocalScalarAssignToBuffer(6.28, (a, b) => Math.Abs(a - b) < 0.001f);
+            LocalScalarAssignToBuffer<Bool>(true, (a, b) => a == b);
         }
 
         [TestMethod]
