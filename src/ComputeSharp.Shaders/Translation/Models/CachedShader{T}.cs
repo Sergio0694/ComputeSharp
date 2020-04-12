a7e3d6bec9a5dcdd15bc0bb901382a96c9ac0c84
Sergio Pedri <sergio0694@live.com>
2020-04-12 20:24:30 +0200

    Added CachedShader<T> type

---
+++ src/ComputeSharp.Shaders/Translation/Models/CachedShader{T}.cs
@@ -0,0 +1,33 @@
+﻿using Vortice.Direct3D12;
+
+namespace ComputeSharp.Shaders.Translation.Models
+{
+    /// <summary>
+    /// A <see langword="struct"/> that contains info on a cached shader
+    /// </summary>
+    /// <typeparam name="T">The type of compute shader in use</typeparam>
+    internal readonly struct CachedShader<T>
+        where T : struct, IComputeShader
+    {
+        /// <summary>
+        /// The <see cref="ShaderLoader"/> instance with the shader metadata
+        /// </summary>
+        public readonly ShaderLoader<T> Loader;
+
+        /// <summary>
+        /// The compiled shader bytecode
+        /// </summary>
+        public readonly ShaderBytecode Bytecode;
+
+        /// <summary>
+        /// Creates a new <see cref="CachedShader"/> instance with the specified parameters
+        /// </summary>
+        /// <param name="loader">The <see cref="ShaderLoader"/> instance with the shader metadata</param>
+        /// <param name="bytecode">The compiled shader bytecode</param>
+        public CachedShader(ShaderLoader<T> loader, ShaderBytecode bytecode)
+        {
+            Loader = loader;
+            Bytecode = bytecode;
+        }
+    }
+}
