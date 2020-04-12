fcf7a9a31dc95f9a80827a44172e19e0c4089167
Sergio Pedri <sergio0694@live.com>
2020-04-12 15:41:58 +0200

    Added IComputeShader interface

---
+++ src/ComputeSharp.Core/Interfaces/IComputeShader.cs
@@ -0,0 +1,14 @@
+﻿namespace ComputeSharp
+{
+    /// <summary>
+    /// An <see langword="interface"/> representing a compute shader.
+    /// </summary>
+    public interface IComputeShader
+    {
+        /// <summary>
+        /// Executes the current compute shader.
+        /// </summary>
+        /// <param name="ids">The <see cref="ThreadIds"/> value with the current execution indices.</param>
+        void Execute(ThreadIds ids);
+    }
+}
