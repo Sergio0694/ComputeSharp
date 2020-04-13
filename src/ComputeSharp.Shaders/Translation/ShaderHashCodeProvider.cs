32ca76c5357580e450213fdade8fa559e7e01aec
Sergio Pedri <sergio0694@live.com>
2020-04-13 03:01:41 +0200

    Removed unnecessary code

--- src/ComputeSharp.Shaders/Translation/ShaderHashCodeProvider.cs
+++ src/ComputeSharp.Shaders/Translation/ShaderHashCodeProvider.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
-using ComputeSharp.Shaders.Mappings;
 
 namespace ComputeSharp.Shaders.Translation
 {
@@ -14,11 +13,6 @@ namespace ComputeSharp.Shaders.Translation
     /// </summary>
     internal static class ShaderHashCodeProvider
     {
-        /// <summary>
-        /// The mapping of hashcodes to aggregate hashing functions
-        /// </summary>
-        private static readonly Dictionary<int, Hasher?> HasherMapping = new Dictionary<int, Hasher?>();
-
         /// <summary>
         /// A type containing static info for a given shader type <typeparamref name="T"/>
         /// </summary>
@@ -122,116 +116,6 @@ namespace ComputeSharp.Shaders.Translation
                 il.Emit(OpCodes.Ldarg_0);
                 il.Emit(OpCodes.Ret);
             });
-        }
-
-        /// <summary>
-        /// Gets a unique hashcode for the input shader closure
-        /// </summary>
-        /// <param name="action">The input <see cref="Action{T}"/> representing the shader to run</param>
-        [Pure]
-        public static int GetHashCode(Action<ThreadIds> action)
-        {
-            // Get the delegate hashcode
-            MethodInfo methodInfo = action.Method;
-            int hash = methodInfo.GetHashCode();
-
-            if (!HasherMapping.TryGetValue(hash, out Hasher? hasher))
-            {
-                // Get the candidate fields for delegate checking
-                FieldInfo[] delegateFieldInfos = (
-                    from fieldInfo in methodInfo.DeclaringType.GetFields()
-                    where fieldInfo.FieldType.IsDelegate() &&
-                          fieldInfo.FieldType.GenericTypeArguments.All(type => HlslKnownTypes.IsKnownScalarType(type) || HlslKnownTypes.IsKnownVectorType(type))
-                    select fieldInfo).ToArray();
-
-                // If at least one captured delegate is present, build the hasher method
-                hasher = delegateFieldInfos.Length == 0 ? null : BuildDynamicHasher(action.Method.DeclaringType, delegateFieldInfos);
-                HasherMapping.Add(hash, hasher);
-            }
-
-            // Aggregate the hash of the captured delegates, if needed
-            if (hasher != null) hash = hasher(hash, action.Target);
-
-            return hash;
-        }
-
-        /// <summary>
-        /// A <see langword="delegate"/> that represents an aggregate hash function for a given instance
-        /// </summary>
-        /// <param name="hash">The initial hash value</param>
-        /// <param name="instance">The closure instance to use to compute the final hash value</param>
-        /// <returns>The final hash value for the input closure</returns>
-        private delegate int Hasher(int hash, object instance);
-
-        /// <summary>
-        /// Builds a new <see cref="Hasher"/> instance for the target <see cref="Type"/> and sequence of <see cref="FieldInfo"/> values
-        /// </summary>
-        /// <param name="declaringType">The declaring <see cref="Type"/> to analyze</param>
-        /// <param name="fieldInfos">The list of captured fields to inspect</param>
-        [Pure]
-        private static Hasher BuildDynamicHasher(Type declaringType, IReadOnlyCollection<FieldInfo> fieldInfos)
-        {
-            return DynamicMethod<Hasher>.New(il =>
-            {
-                // Declare the local variable to store the target instance
-                il.DeclareLocal(declaringType);
-
-                // Load the argument (the object instance)
-                il.Emit(OpCodes.Ldarg_1);
-                il.Emit(OpCodes.Castclass, declaringType);
-                il.Emit(OpCodes.Stloc_0);
-
-                // Unroll the member access
-                MethodInfo
-                    validateDelegateInfo = typeof(ShaderHashCodeProvider).GetMethod(nameof(ValidateDelegate), BindingFlags.NonPublic | BindingFlags.Static),
-                    getMethodInfo = typeof(Delegate).GetProperty(nameof(Delegate.Method)).GetMethod,
-                    getHashCodeInfo = typeof(object).GetMethod(nameof(object.GetHashCode));
-                foreach (FieldInfo fieldInfo in fieldInfos)
-                {
-                    Label label = il.DefineLabel();
-
-                    // if (fieldInfo.GetValue(obj) != null) {
-                    il.Emit(OpCodes.Ldloc_0);
-                    il.Emit(OpCodes.Ldfld, fieldInfo);
-                    il.Emit(OpCodes.Brfalse_S, label);
-
-                    // if (ValidateDelegate(field)) {
-                    il.Emit(OpCodes.Ldloc_0);
-                    il.Emit(OpCodes.Ldfld, fieldInfo);
-                    il.EmitCall(OpCodes.Call, validateDelegateInfo, null);
-                    il.Emit(OpCodes.Brfalse_S, label);
-
-                    // hash = hash * 17 + field.Method.GetHashCode();
-                    il.Emit(OpCodes.Ldarg_0);
-                    il.Emit(OpCodes.Ldc_I4_S, (byte)17);
-                    il.Emit(OpCodes.Mul);
-                    il.Emit(OpCodes.Ldloc_0);
-                    il.Emit(OpCodes.Ldfld, fieldInfo);
-                    il.EmitCall(OpCodes.Callvirt, getMethodInfo, null);
-                    il.EmitCall(OpCodes.Callvirt, getHashCodeInfo, null);
-                    il.Emit(OpCodes.Add);
-                    il.Emit(OpCodes.Starg_S, (byte)0);
-
-                    il.MarkLabel(label);
-                }
-
-                // Return the computed hash
-                il.Emit(OpCodes.Ldarg_0);
-                il.Emit(OpCodes.Ret);
-            });
-        }
-
-        /// <summary>
-        /// A method that checks whether an input <see cref="Delegate"/> is supported in HLSL
-        /// </summary>
-        /// <param name="candidate">The input <see cref="Delegate"/> instance to check</param>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static bool ValidateDelegate(Delegate candidate)
-        {
-            MethodInfo methodInfo = candidate.Method;
-            return (methodInfo.IsStatic || methodInfo.DeclaringType.IsStatelessDelegateContainer()) &&
-                   (HlslKnownTypes.IsKnownScalarType(methodInfo.ReturnType) || HlslKnownTypes.IsKnownVectorType(methodInfo.ReturnType));
         }
     }
 }
--- src/ComputeSharp/Extensions/GraphicsDeviceExtensions.cs
+++ src/ComputeSharp/Extensions/GraphicsDeviceExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ComputeSharp.Graphics;
+﻿using ComputeSharp.Graphics;
 using ComputeSharp.Shaders;
 
 namespace ComputeSharp
@@ -9,50 +8,6 @@ namespace ComputeSharp
     /// </summary>
     public static class GraphicsDeviceExtensions
     {
-        /// <summary>
-        /// Compiles and runs the input shader on a target <see cref="GraphicsDevice"/> instance, with the specified parameters
-        /// </summary>
-        /// <param name="device">The <see cref="GraphicsDevice"/> to use to run the shader</param>
-        /// <param name="x">The number of iterations to run on the X axis</param>
-        /// <param name="action">The input <see cref="Action{T}"/> representing the compute shader to run</param>
-        public static void For(this GraphicsDevice device, int x, Action<ThreadIds> action) => ShaderRunner.Run(device, x, 1, 1, action);
-
-        /// <summary>
-        /// Compiles and runs the input shader on a target <see cref="GraphicsDevice"/> instance, with the specified parameters
-        /// </summary>
-        /// <param name="device">The <see cref="GraphicsDevice"/> to use to run the shader</param>
-        /// <param name="x">The number of iterations to run on the X axis</param>
-        /// <param name="y">The number of iterations to run on the Y axis</param>
-        /// <param name="action">The input <see cref="Action{T}"/> representing the compute shader to run</param>
-        public static void For(this GraphicsDevice device, int x, int y, Action<ThreadIds> action) => ShaderRunner.Run(device, x, y, 1, action);
-
-        /// <summary>
-        /// Compiles and runs the input shader on a target <see cref="GraphicsDevice"/> instance, with the specified parameters
-        /// </summary>
-        /// <param name="device">The <see cref="GraphicsDevice"/> to use to run the shader</param>
-        /// <param name="x">The number of iterations to run on the X axis</param>
-        /// <param name="y">The number of iterations to run on the Y axis</param>
-        /// <param name="z">The number of iterations to run on the Z axis</param>
-        /// <param name="action">The input <see cref="Action{T}"/> representing the compute shader to run</param>
-        public static void For(this GraphicsDevice device, int x, int y, int z, Action<ThreadIds> action) => ShaderRunner.Run(device, x, y, z, action);
-
-        /// <summary>
-        /// Compiles and runs the input shader on a target <see cref="GraphicsDevice"/> instance, with the specified parameters
-        /// </summary>
-        /// <param name="device">The <see cref="GraphicsDevice"/> to use to run the shader</param>
-        /// <param name="x">The number of iterations to run on the X axis</param>
-        /// <param name="y">The number of iterations to run on the Y axis</param>
-        /// <param name="z">The number of iterations to run on the Z axis</param>
-        /// <param name="threadsX">The number of threads in each thread group for the X axis</param>
-        /// <param name="threadsY">The number of threads in each thread group for the Y axis</param>
-        /// <param name="threadsZ">The number of threads in each thread group for the Z axis</param>
-        /// <param name="action">The input <see cref="Action{T}"/> representing the compute shader to run</param>
-        public static void For(
-            this GraphicsDevice device,
-            int x, int y, int z,
-            int threadsX, int threadsY, int threadsZ,
-            Action<ThreadIds> action) => ShaderRunner.Run(device, x, y, z, threadsX, threadsY, threadsZ, action);
-
         /// <summary>
         /// Compiles and runs the input shader on a target <see cref="GraphicsDevice"/> instance, with the specified parameters
         /// </summary>
