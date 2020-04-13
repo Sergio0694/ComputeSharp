bc49147e8e77d37ef99fad81cfc4f637cd3c4656
Sergio Pedri <sergio0694@live.com>
2020-04-13 02:57:59 +0200

    Fixed shader not loading non public instance members

--- src/ComputeSharp.Shaders/Translation/ShaderLoader{T}.cs
+++ src/ComputeSharp.Shaders/Translation/ShaderLoader{T}.cs
@@ -125,7 +125,7 @@ namespace ComputeSharp.Shaders.Translation
         /// <param name="shader">The boxed <typeparamref name="T"/> instance to use to build the shader</param>
         private void LoadFieldsInfo(object shader)
         {
-            IReadOnlyList<FieldInfo> shaderFields = typeof(T).GetFields().ToArray();
+            IReadOnlyList<FieldInfo> shaderFields = typeof(T).GetFields(BindingFlags.Instance | BindingFlags.NonPublic).ToArray();
 
             if (shaderFields.Count == 0) throw new InvalidOperationException("Empty shader body");
 
