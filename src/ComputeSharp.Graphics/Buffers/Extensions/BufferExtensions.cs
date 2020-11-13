10b96809e631375e686ae9c5d2488147d62866ef
Sergio Pedri <sergio0694@live.com>
2020-11-13 20:05:36 +0100

    Removed external dependency and few extensions

--- src/ComputeSharp.Graphics/Buffers/Extensions/BufferExtensions.cs
+++ src/ComputeSharp.Graphics/Buffers/Extensions/BufferExtensions.cs
@@ -3,7 +3,6 @@ using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
 using ComputeSharp.Graphics;
 using ComputeSharp.Graphics.Buffers.Abstract;
-using Microsoft.Toolkit.HighPerformance.Extensions;
 
 namespace ComputeSharp
 {
@@ -22,20 +21,6 @@ namespace ComputeSharp
         [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ConstantBuffer<T> AllocateConstantBuffer<T>(this GraphicsDevice device, T[] array) where T : unmanaged
-        {
-            return device.AllocateConstantBuffer(array.AsSpan());
-        }
-
-        /// <summary>
-        /// Allocates a new constant buffer with the specified parameters
-        /// </summary>
-        /// <typeparam name="T">The type of items to store in the buffer</typeparam>
-        /// <param name="device">The <see cref="GraphicsDevice"/> instance to use to allocate the buffer</param>
-        /// <param name="array">The input 2D <typeparamref name="T"/> array with the data to copy on the allocated buffer</param>
-        /// <returns>A constant <see cref="ConstantBuffer{T}"/> instance with the contents of the input 2D array</returns>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ConstantBuffer<T> AllocateConstantBuffer<T>(this GraphicsDevice device, T[,] array) where T : unmanaged
         {
             return device.AllocateConstantBuffer(array.AsSpan());
         }
@@ -107,20 +92,6 @@ namespace ComputeSharp
         /// </summary>
         /// <typeparam name="T">The type of items to store in the buffer</typeparam>
         /// <param name="device">The <see cref="GraphicsDevice"/> instance to use to allocate the buffer</param>
-        /// <param name="array">The input 2D <typeparamref name="T"/> array with the data to copy on the allocated buffer</param>
-        /// <returns>A read write <see cref="ReadOnlyBuffer{T}"/> instance with the contents of the input 2D array</returns>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ReadOnlyBuffer<T> AllocateReadOnlyBuffer<T>(this GraphicsDevice device, T[,] array) where T : unmanaged
-        {
-            return device.AllocateReadOnlyBuffer(array.AsSpan());
-        }
-
-        /// <summary>
-        /// Allocates a new readonly buffer with the specified parameters
-        /// </summary>
-        /// <typeparam name="T">The type of items to store in the buffer</typeparam>
-        /// <param name="device">The <see cref="GraphicsDevice"/> instance to use to allocate the buffer</param>
         /// <param name="span">The input <see cref="Span{T}"/> with the data to copy on the allocated buffer</param>
         /// <returns>A read write <see cref="ReadOnlyBuffer{T}"/> instance with the contents of the input <see cref="Span{T}"/></returns>
         [Pure]
@@ -174,20 +145,6 @@ namespace ComputeSharp
         [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ReadWriteBuffer<T> AllocateReadWriteBuffer<T>(this GraphicsDevice device, T[] array) where T : unmanaged
-        {
-            return device.AllocateReadWriteBuffer(array.AsSpan());
-        }
-
-        /// <summary>
-        /// Allocates a new read write buffer with the specified parameters
-        /// </summary>
-        /// <typeparam name="T">The type of items to store in the buffer</typeparam>
-        /// <param name="device">The <see cref="GraphicsDevice"/> instance to use to allocate the buffer</param>
-        /// <param name="array">The input 2D <typeparamref name="T"/> array with the data to copy on the allocated buffer</param>
-        /// <returns>A read write <see cref="ReadWriteBuffer{T}"/> instance with the contents of the input 2D array</returns>
-        [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ReadWriteBuffer<T> AllocateReadWriteBuffer<T>(this GraphicsDevice device, T[,] array) where T : unmanaged
         {
             return device.AllocateReadWriteBuffer(array.AsSpan());
         }
