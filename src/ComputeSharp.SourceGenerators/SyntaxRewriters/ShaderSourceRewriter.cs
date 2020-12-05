f2e93a6c88377c9ae5bc1f0ccacc0aae4975af5c
Sergio Pedri <sergio0694@live.com>
2020-12-06 00:23:07 +0100

    Fixed a bug in the ObjectCreationExpressionSyntax visitor

--- src/ComputeSharp.SourceGenerators/SyntaxRewriters/ShaderSourceRewriter.cs
+++ src/ComputeSharp.SourceGenerators/SyntaxRewriters/ShaderSourceRewriter.cs
@@ -158,7 +158,7 @@ namespace ComputeSharp.SourceGenerators.SyntaxRewriters
         {
             var updatedNode = (ObjectCreationExpressionSyntax)base.VisitObjectCreationExpression(node)!;
 
-            updatedNode = updatedNode.ReplaceAndTrackType(updatedNode.Type, node.Type, this.semanticModel, this.discoveredTypes);
+            updatedNode = updatedNode.ReplaceAndTrackType(updatedNode.Type, node, this.semanticModel, this.discoveredTypes);
 
             // New objects use the default HLSL cast syntax, eg. (float4)0
             if (updatedNode.ArgumentList!.Arguments.Count == 0)
