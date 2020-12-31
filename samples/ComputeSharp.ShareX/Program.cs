2dd0549eecefab4aa40bccb16e8f1c4b73f4dccc
Sergio Pedri <sergio0694@live.com>
2020-12-31 01:31:36 +0100

    Implemented ShareX sample

--- samples/ComputeSharp.ShareX/Program.cs
+++ samples/ComputeSharp.ShareX/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using ShareX.HelpersLib;
 
 namespace ComputeSharp.ShareX
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(">> Loading image");
+
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "city.jpg");
+
+            using Bitmap sourceBitmap = new(path);
+
+            Console.WriteLine(">> Blurring on CPU");
+
+            using Bitmap resultBitmapCpu = ConvolutionMatrixManager.GaussianBlur(60, 60, 10).Apply(sourceBitmap);
+
+            string resultPathCpu = Path.Combine(
+                Path.GetRelativePath(Path.GetDirectoryName(path)!, @"..\..\..\"),
+                $"{Path.GetFileNameWithoutExtension(path)}_cpu{Path.GetExtension(path)}");
+
+            Console.WriteLine(">> Saving image");
+
+            resultBitmapCpu.Save(resultPathCpu);
+
+            Console.WriteLine(">> Blurring on GPU");
+
+            using Bitmap resultBitmapGpu = HlslConvolutionManager.Create(60, 10).Apply(sourceBitmap);
+
+            string resultPathGpu = Path.Combine(
+                Path.GetRelativePath(Path.GetDirectoryName(path)!, @"..\..\..\"),
+                $"{Path.GetFileNameWithoutExtension(path)}_gpu{Path.GetExtension(path)}");
+
+            Console.WriteLine(">> Saving image");
+
+            resultBitmapGpu.Save(resultPathGpu);
         }
     }
 }
