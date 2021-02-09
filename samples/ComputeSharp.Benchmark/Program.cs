7cad8dae60fb3a5f78b921ab216cb75eb16a5e79
Sergio Pedri <sergio0694@live.com>
2021-02-10 00:30:07 +0100

    Updated NuGet packages, fixed analyzer exceptions

--- samples/ComputeSharp.Benchmark/Program.cs
+++ samples/ComputeSharp.Benchmark/Program.cs
@@ -1,8 +1,4 @@
-﻿using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Jobs;
-using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Toolchains.CsProj;
-using BenchmarkDotNet.Toolchains.DotNetCli;
+﻿using BenchmarkDotNet.Running;
 
 namespace ComputeSharp.Benchmark
 {
@@ -10,23 +6,13 @@ namespace ComputeSharp.Benchmark
     {
         public static void Main(string[] args)
         {
-            ManualConfig configuration =
-                DefaultConfig.Instance.AddJob(
-                    Job.Default.WithToolchain(
-                        CsProjCoreToolchain.From(
-                            new NetCoreAppSettings(
-                                targetFrameworkMoniker: "net5.0-windows10.0.19041.0",
-                                runtimeFrameworkVersion: null,
-                                name: "5.0")))
-                    .AsDefault());
-
             // =================
             // BenchmarkDotNet
             // ================
             // In order to run this benchmark, compile this project in Release mode,
             // then go to its bin\Release\netcoreapp3.0 folder, open a cmd prompt
             // and type "dotnet ComputeSharp.Benchmark.dll" to execute it.
-            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, configuration);
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
 }
