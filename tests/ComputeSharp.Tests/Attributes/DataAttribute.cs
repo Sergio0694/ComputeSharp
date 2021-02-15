088b46e69c3664815a07c92b05609649da187a0e
Sergio Pedri <sergio0694@live.com>
2021-02-15 23:00:20 +0100

    Added new test attributes and extensions

---
+++ tests/ComputeSharp.Tests/Attributes/DataAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ComputeSharp.Tests.Attributes
+{
+    /// <summary>
+    /// An attribute to use with <see cref="CombinatorialAttribute"/> adding arbitrary input data.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class DataAttribute : Attribute
+    {
+        public DataAttribute(params object[] data)
+        {
+            Data = data;
+        }
+
+        /// <summary>
+        /// Gets the input data for the current attribute.
+        /// </summary>
+        public object[] Data { get; }
+    }
+}
