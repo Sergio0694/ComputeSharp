de7bb4911fad8a937c746d8d3719b78303ba59b1
Sergio Pedri <sergio0694@live.com>
2021-03-03 17:50:31 +0100

    Added initial T4 template for HLSL intrinsics

---
+++ src/ComputeSharp/Core/Intrinsics/Attributes/HlslIntrinsicNameAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ComputeSharp.Core.Intrinsics.Attributes
+{
+    /// <summary>
+    /// An attribute indicating the native member name of a given HLSL intrinsic.
+    /// </summary>
+    internal sealed class HlslIntrinsicNameAttribute : Attribute
+    {
+        /// <summary>
+        /// Creates a new <see cref="HlslIntrinsicNameAttribute"/> instance with the specified parameters.
+        /// </summary>
+        /// <param name="name">The name of the HLSL intrinsic.</param>
+        public HlslIntrinsicNameAttribute(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// Gets the name of the HLSL intrinsic.
+        /// </summary>
+        public string Name { get; }
+    }
+}
