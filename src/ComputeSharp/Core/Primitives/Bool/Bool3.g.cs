b111e450e42caff36f410480e5f379c135973eff
Sergio Pedri <sergio0694@live.com>
2021-03-16 16:06:14 +0100

    Removed unnecessary using statements in Bool vector types

--- src/ComputeSharp/Core/Primitives/Bool/Bool3.g.cs
+++ src/ComputeSharp/Core/Primitives/Bool/Bool3.g.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 #if !NET5_0
