95771a5a92ce55ed2457432e2c6fac463343c982
Sergio Pedri <sergio0694@live.com>
2021-04-28 21:58:10 +0200

    Enabled initial setup for WinUI 3 sample

--- samples/ComputeSharp.SwapChain.WinUI/ComputeSharp.SwapChain.WinUI/MainWindow.xaml.cs
+++ samples/ComputeSharp.SwapChain.WinUI/ComputeSharp.SwapChain.WinUI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml;
+﻿using ComputeSharp.SwapChain.Shaders;
+using ComputeSharp.WinUI;
+using Microsoft.UI.Xaml;
 
 namespace ComputeSharp.SwapChain.WinUI
 {
@@ -12,9 +14,39 @@ namespace ComputeSharp.SwapChain.WinUI
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void Colors_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            this.ShaderPanel.ShaderRunner = new ShaderRunner<ColorfulInfinity>(static (texture, time) => new(texture, (float)time.TotalSeconds));
+        }
+
+        private void ExtrudedTruchet_Click(object sender, RoutedEventArgs e)
+        {
+            this.ShaderPanel.ShaderRunner = new ShaderRunner<ExtrudedTruchetPattern>(static (texture, time) => new(texture, (float)time.TotalSeconds));
+        }
+
+        private void Tiles_Click(object sender, RoutedEventArgs e)
+        {
+            this.ShaderPanel.ShaderRunner = new ShaderRunner<FractalTiling>(static (texture, time) => new(texture, (float)time.TotalSeconds));
+        }
+
+        private void Journey_Click(object sender, RoutedEventArgs e)
+        {
+            this.ShaderPanel.ShaderRunner = new ShaderRunner<MengerJourney>(static (texture, time) => new(texture, (float)time.TotalSeconds));
+        }
+
+        private void Octagrams_Click(object sender, RoutedEventArgs e)
+        {
+            this.ShaderPanel.ShaderRunner = new ShaderRunner<Octagrams>(static (texture, time) => new(texture, (float)time.TotalSeconds));
+        }
+
+        private void Clouds_Click(object sender, RoutedEventArgs e)
+        {
+            this.ShaderPanel.ShaderRunner = new ShaderRunner<ProteanClouds>(static (texture, time) => new(texture, (float)time.TotalSeconds));
+        }
+
+        private void TiledTruched_Click(object sender, RoutedEventArgs e)
+        {
+            this.ShaderPanel.ShaderRunner = new ShaderRunner<TwoTiledTruchet>(static (texture, time) => new(texture, (float)time.TotalSeconds));
         }
     }
 }
