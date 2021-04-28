4bb114d70b42478517bc9564cc8811647b14652e
Sergio Pedri <sergio0694@live.com>
2021-04-28 21:48:03 +0200

    Added initial ComputeSharp.WinUI project

---
+++ src/ComputeSharp.WinUI/ComputeShaderPanel.Properties.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace ComputeSharp.WinUI
+{
+    /// <inheritdoc cref="ComputeShaderPanel"/>
+    public sealed unsafe partial class ComputeShaderPanel : SwapChainPanel
+    {
+        /// <summary>
+        /// Gets or sets the <see cref="IShaderRunner"/> instance to use to render content.
+        /// </summary>
+        public IShaderRunner? ShaderRunner
+        {
+            get => (IShaderRunner?)GetValue(ShaderFactoryProperty);
+            set => SetValue(ShaderFactoryProperty, value);
+        }
+
+        /// <summary>
+        /// The <see cref="DependencyProperty"/> backing <see cref="ShaderRunner"/>.
+        /// </summary>
+        public static readonly DependencyProperty ShaderFactoryProperty = DependencyProperty.Register(
+            nameof(ShaderRunner),
+            typeof(IShaderRunner),
+            typeof(ComputeShaderPanel),
+            new PropertyMetadata(null, static (d, e) => ((ComputeShaderPanel)d).shaderRunner = (IShaderRunner?)e.NewValue));
+    }
+}
