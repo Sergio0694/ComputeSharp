139a278488d9923616c88132476608455b418512
Sergio Pedri <sergio0694@live.com>
2021-04-28 21:54:59 +0200

    Added empty WinUI 3 sample

---
+++ samples/ComputeSharp.SwapChain.WinUI/ComputeSharp.SwapChain.WinUI/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace ComputeSharp.SwapChain.WinUI
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+        }
+    }
+}
