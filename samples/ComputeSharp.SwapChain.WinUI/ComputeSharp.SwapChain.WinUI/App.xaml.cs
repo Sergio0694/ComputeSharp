6eecf3cac874871ae9fd8194047c3c725880bfda
Sergio Pedri <sergio0694@live.com>
2021-04-28 21:54:59 +0200

    Added empty WinUI 3 sample

---
+++ samples/ComputeSharp.SwapChain.WinUI/ComputeSharp.SwapChain.WinUI/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Xaml;
+
+#nullable enable
+
+namespace ComputeSharp.SwapChain.WinUI
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        private Window? m_window;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <inheritdoc/>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+            m_window.Activate();
+        }
+    }
+}
