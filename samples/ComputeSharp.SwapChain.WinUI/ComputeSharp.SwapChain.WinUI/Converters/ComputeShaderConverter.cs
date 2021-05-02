5bc0ba1cf44cf200952f0b94b48fbf67fab3bc45
Sergio Pedri <sergio0694@live.com>
2021-05-02 19:57:51 +0200

    Improved sample UI, implemented shader selector

---
+++ samples/ComputeSharp.SwapChain.WinUI/ComputeSharp.SwapChain.WinUI/Converters/ComputeShaderConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ComputeSharp.SwapChain.WinUI.Models;
+
+namespace ComputeSharp.SwapChain.WinUI.Converters
+{
+    /// <summary>
+    /// A class with some static converters for <see cref="ComputeShader"/>.
+    /// </summary>
+    public static class ComputeShaderConverter
+    {
+        /// <summary>
+        /// Converts a <see cref="ComputeShader"/> instance to a <see cref="Uri"/> with the thumbnail image.
+        /// </summary>
+        /// <param name="value">The input <see cref="ComputeShader"/> instance.</param>
+        /// <returns>A <see cref="Uri"/> with the thumbnail image for <paramref name="value"/>.</returns>
+        public static Uri ConvertComputeShaderToThumbnailUri(ComputeShader value)
+        {
+            return new($"ms-appx:///Images/Shaders/{value.ShaderRunner.GetType().GenericTypeArguments[0].Name}.png");
+        }
+
+        /// <summary>
+        /// Converts a <see cref="ComputeShader"/> instance to a <see cref="Uri"/> with the background image.
+        /// </summary>
+        /// <param name="value">The input <see cref="ComputeShader"/> instance.</param>
+        /// <returns>A <see cref="Uri"/> with the background image for <paramref name="value"/>.</returns>
+        public static Uri ConvertComputeShaderToBackgroundUri(ComputeShader value)
+        {
+            return new($"ms-appx:///Images/Shaders/Blurred/{value.ShaderRunner.GetType().GenericTypeArguments[0].Name}.png");
+        }
+    }
+}
---
+++ samples/ComputeSharp.SwapChain.WinUI/ComputeSharp.SwapChain.WinUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using ComputeSharp.SwapChain.Shaders;
+using ComputeSharp.WinUI;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ComputeSharp.SwapChain.WinUI.Models;
+
+namespace ComputeSharp.SwapChain.WinUI.ViewModels
+{
+    /// <summary>
+    /// The viewmodel for for a view that allows users to select shaders and customize settings.
+    /// </summary>
+    public sealed class MainViewModel : ObservableObject
+    {
+        /// <summary>
+        /// Creates a new <see cref="MainViewModel"/> instance.
+        /// </summary>
+        public MainViewModel()
+        {
+            this.selectedResolutionScale = 100;
+            this.selectedComputeShader = ComputeShaderOptions[0];
+            this.selectedComputeShader.IsSelected = true;
+        }
+
+        /// <summary>
+        /// Gets the available resolution scaling options (as percentage values).
+        /// </summary>
+        public IReadOnlyList<int> ResolutionScaleOptions { get; } = new[] { 25, 50, 75, 100 };
+
+        private int selectedResolutionScale;
+
+        /// <summary>
+        /// Gets or sets the currently selected resolution scale setting (as percentage valaue).
+        /// </summary>
+        public int SelectedResolutionScale
+        {
+            get => this.selectedResolutionScale;
+            set => SetProperty(ref this.selectedResolutionScale, value);
+        }
+
+        /// <summary>
+        /// Gets the collection of available compute shader.
+        /// </summary>
+        public IReadOnlyList<ComputeShader> ComputeShaderOptions { get; } = new ComputeShader[]
+        {
+            new("Colorful infinity", new ShaderRunner<ColorfulInfinity>(static (texture, time) => new(texture, (float)time.TotalSeconds))),
+            new("Extruded truchet", new ShaderRunner<ExtrudedTruchetPattern>(static (texture, time) => new(texture, (float)time.TotalSeconds))),
+            new("Fractal tiling", new ShaderRunner<FractalTiling>(static (texture, time) => new(texture, (float)time.TotalSeconds))),
+            new("Menger Journey", new ShaderRunner<MengerJourney>(static (texture, time) => new(texture, (float)time.TotalSeconds))),
+            new("Octagrams", new ShaderRunner<Octagrams>(static (texture, time) => new(texture, (float)time.TotalSeconds))),
+            new("Protean clouds", new ShaderRunner<ProteanClouds>(static (texture, time) => new(texture, (float)time.TotalSeconds))),
+            new("Two tiled truchet", new ShaderRunner<TwoTiledTruchet>(static (texture, time) => new(texture, (float)time.TotalSeconds)))
+        };
+
+        private ComputeShader selectedComputeShader;
+
+        /// <summary>
+        /// Gets or sets the currently selected compute shader.
+        /// </summary>
+        public ComputeShader SelectedComputeShader
+        {
+            get => this.selectedComputeShader;
+            set
+            {
+                this.selectedComputeShader.IsSelected = false;
+
+                if (SetProperty(ref this.selectedComputeShader, value) &&
+                    value is not null)
+                {
+                    value.IsSelected = true;
+                }
+            }
+        }
+    }
+}
