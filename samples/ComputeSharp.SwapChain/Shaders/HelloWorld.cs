aebdf002d6e8c8f4579e8cf75e480806be9ea373
patan77 <patan77@live.se>
2021-10-19 11:47:15 +0200

    Added a simple "HelloWorld" shader based on shadertoy/new to the SwapChain sample shaders to make it easier to understand and get started with making custom shaders.

---
+++ samples/ComputeSharp.SwapChain/Shaders/HelloWorld.cs
@@ -0,0 +1,29 @@
+﻿namespace ComputeSharp.SwapChain.Shaders
+{
+    /// <summary>
+    /// A simple shader to get started with based on shadertoy new shader template.
+    /// Ported from <see href="https://www.shadertoy.com/new"/>.
+    /// </summary>
+    /// 
+    [AutoConstructor]
+    internal readonly partial struct HelloWorld : IPixelShader<Float4>
+    {
+        /// <summary>
+        /// The current time since the start of the application.
+        /// </summary>
+        public readonly float time;
+
+        /// <inheritdoc/>
+        public Float4 Execute()
+        {
+            // Normalized ScreenSpace UV coordinates from 0.0f to 1.0f
+            Float2 uv = new Float2(ThreadIds.Normalized.X, ThreadIds.Normalized.Y);
+
+            // Time varying pixel color
+            Float3 col = 0.5f + 0.5f * Hlsl.Cos(time + new Float3(uv, uv.X) + new Float3(0, 2, 4));
+
+            // Output to screen
+            return new(col, 1f);
+        }
+    }
+}
