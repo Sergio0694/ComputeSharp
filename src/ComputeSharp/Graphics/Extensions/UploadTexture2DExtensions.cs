16c067ed3492101d88c62b9fd9ad7176383880cc
Sergio Pedri <sergio0694@live.com>
2021-11-12 02:48:15 +0100

    Add missing transfer 2D texture copy APIs

--- src/ComputeSharp/Graphics/Extensions/UploadTexture2DExtensions.cs
+++ src/ComputeSharp/Graphics/Extensions/UploadTexture2DExtensions.cs
@@ -34,4 +34,22 @@ public static class UploadTexture2DExtensions
     {
         destination.CopyFrom(source, sourceOffsetX, sourceOffsetY, 0, 0, width, height);
     }
+
+    /// <summary>
+    /// Reads the contents of a <see cref="UploadTexture2D{T}"/> instance and writes them into a target <see cref="Texture2D{T}"/> instance.
+    /// </summary>
+    /// <typeparam name="T">The type of items stored on the texture.</typeparam>
+    /// <param name="source">The input <see cref="UploadTexture2D{T}"/> instance to read data from.</param>
+    /// <param name="destination">The target <see cref="Texture2D{T}"/> instance to write data to.</param>
+    /// <param name="sourceOffsetX">The horizontal offset in the source texture.</param>
+    /// <param name="sourceOffsetY">The vertical offset in the source texture.</param>
+    /// <param name="destinationOffsetX">The horizontal offset in the destination texture.</param>
+    /// <param name="destinationOffsetY">The vertical offset in the destination texture.</param>
+    /// <param name="width">The width of the memory area to copy.</param>
+    /// <param name="height">The height of the memory area to copy.</param>
+    public static void CopyTo<T>(this UploadTexture2D<T> source, Texture2D<T> destination, int sourceOffsetX, int sourceOffsetY, int destinationOffsetX, int destinationOffsetY, int width, int height)
+        where T : unmanaged
+    {
+        destination.CopyFrom(source, sourceOffsetX, sourceOffsetY, destinationOffsetX, destinationOffsetY, width, height);
+    }
 }
