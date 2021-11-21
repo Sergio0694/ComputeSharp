a5f0045de8da40156372b329a3d7e104d2b5ce0a
Sergio Pedri <sergio0694@live.com>
2021-11-21 16:14:50 +0100

    Switch Marshal.AllocHGlobal to NativeMemory.Alloc

--- src/ComputeSharp/Graphics/Helpers/DeviceHelper.ID3D12InfoQueue.cs
+++ src/ComputeSharp/Graphics/Helpers/DeviceHelper.ID3D12InfoQueue.cs
@@ -41,18 +41,23 @@ internal static partial class DeviceHelper
                     nuint length;
                     queue.Get()->GetMessage(i, null, &length);
 
-                    D3D12_MESSAGE* message = (D3D12_MESSAGE*)Marshal.AllocHGlobal((nint)length);
+                    D3D12_MESSAGE* message = (D3D12_MESSAGE*)NativeMemory.Alloc(length);
 
-                    queue.Get()->GetMessage(i, message, &length);
+                    try
+                    {
+                        queue.Get()->GetMessage(i, message, &length);
 
-                    builder.Clear();
-                    builder.AppendLine($"[D3D12 message #{i} for \"{device}\" (HW: {device.IsHardwareAccelerated}, UMA: {device.IsCacheCoherentUMA})]");
-                    builder.AppendLine($"[Category]: {Enum.GetName(message->Category)}");
-                    builder.AppendLine($"[Severity]: {Enum.GetName(message->Severity)}");
-                    builder.AppendLine($"[ID]: {Enum.GetName(message->ID)}");
-                    builder.Append($"[Description]: \"{new string(message->pDescription)}\"");
-
-                    Marshal.FreeHGlobal((IntPtr)message);
+                        builder.Clear();
+                        builder.AppendLine($"[D3D12 message #{i} for \"{device}\" (HW: {device.IsHardwareAccelerated}, UMA: {device.IsCacheCoherentUMA})]");
+                        builder.AppendLine($"[Category]: {Enum.GetName(message->Category)}");
+                        builder.AppendLine($"[Severity]: {Enum.GetName(message->Severity)}");
+                        builder.AppendLine($"[ID]: {Enum.GetName(message->ID)}");
+                        builder.Append($"[Description]: \"{new string(message->pDescription)}\"");
+                    }
+                    finally
+                    {
+                        NativeMemory.Free(message);
+                    }
 
                     if (message->Severity is D3D12_MESSAGE_SEVERITY_ERROR or D3D12_MESSAGE_SEVERITY_CORRUPTION or D3D12_MESSAGE_SEVERITY_WARNING)
                     {
