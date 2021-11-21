c6252ee1c9041e6f4a407816e551455c9d545a53
Sergio Pedri <sergio0694@live.com>
2021-11-21 13:13:00 +0100

    Remove leftover nested namespace blocks

--- tests/ComputeSharp.Tests/Shaders/Compute/HelloWorld.cs
+++ tests/ComputeSharp.Tests/Shaders/Compute/HelloWorld.cs
@@ -1,28 +1,27 @@
-﻿namespace ComputeSharp.SwapChain.Shaders.Compute
+﻿namespace ComputeSharp.SwapChain.Shaders.Compute;
+
+/// <summary>
+/// A simple shader to get started with based on shadertoy new shader template.
+/// Ported from <see href="https://www.shadertoy.com/new"/>.
+/// </summary>
+[AutoConstructor]
+internal readonly partial struct HelloWorld : IPixelShader<float4>
 {
     /// <summary>
-    /// A simple shader to get started with based on shadertoy new shader template.
-    /// Ported from <see href="https://www.shadertoy.com/new"/>.
+    /// The current time since the start of the application.
     /// </summary>
-    [AutoConstructor]
-    internal readonly partial struct HelloWorld : IPixelShader<float4>
+    public readonly float time;
+
+    /// <inheritdoc/>
+    public float4 Execute()
     {
-        /// <summary>
-        /// The current time since the start of the application.
-        /// </summary>
-        public readonly float time;
+        // Normalized screen space UV coordinates from 0.0 to 1.0
+        float2 uv = ThreadIds.Normalized.XY;
 
-        /// <inheritdoc/>
-        public float4 Execute()
-        {
-            // Normalized screen space UV coordinates from 0.0 to 1.0
-            float2 uv = ThreadIds.Normalized.XY;
+        // Time varying pixel color
+        float3 col = 0.5f + 0.5f * Hlsl.Cos(time + new float3(uv, uv.X) + new float3(0, 2, 4));
 
-            // Time varying pixel color
-            float3 col = 0.5f + 0.5f * Hlsl.Cos(time + new float3(uv, uv.X) + new float3(0, 2, 4));
-
-            // Output to screen
-            return new(col, 1f);
-        }
+        // Output to screen
+        return new(col, 1f);
     }
 }
