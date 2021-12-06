ea9f568ee45d2a86bcf2b97ca56680a347755d4d
Sergio Pedri <sergio0694@live.com>
2021-12-06 13:01:41 +0100

    Add HashCode.AddRange(...) extension

---
+++ src/ComputeSharp.SourceGenerators/Extensions/HashCodeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace ComputeSharp.SourceGenerators.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="HashCode"/>.
+/// </summary>
+internal static class HashCodeExtensions
+{
+    /// <summary>
+    /// Adds all items from a given <see cref="ImmutableArray{T}"/> instance to an hashcode.
+    /// </summary>
+    /// <typeparam name="T">The type of items to hash.</typeparam>
+    /// <param name="hashCode">The target <see cref="HashCode"/> instance.</param>
+    /// <param name="items">The input items to hash.</param>
+    public static void AddRange<T>(this ref HashCode hashCode, ImmutableArray<T> items)
+    {
+        foreach (T item in items)
+        {
+            hashCode.Add(item);
+        }
+    }
+}
