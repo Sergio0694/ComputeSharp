9205b38de79d40427f8e1aa56a29553f5f428e88
Sergio Pedri <sergio0694@live.com>
2022-01-01 17:50:34 +0100

    Add [SkipLocalsInit] to UWP sample app

---
+++ samples/ComputeSharp.SwapChain.Uwp/Properties/SkipLocalsInitAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace System.Runtime.CompilerServices;
+
+/// <summary>
+/// Used to indicate to the compiler that the <c>.locals init</c> flag should not be set in method headers.
+/// </summary>
+[AttributeUsage(AttributeTargets.Module
+    | AttributeTargets.Class
+    | AttributeTargets.Struct
+    | AttributeTargets.Interface
+    | AttributeTargets.Constructor
+    | AttributeTargets.Method
+    | AttributeTargets.Property
+    | AttributeTargets.Event, Inherited = false)]
+internal sealed class SkipLocalsInitAttribute : Attribute
+{
+}
