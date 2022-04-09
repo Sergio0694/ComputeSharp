2c5acc0747e13a6cf8609199888b699951da3426
Sergio Pedri <sergio0694@live.com>
2022-04-09 17:21:46 +0200

    Remove leftover types from initial port

--- src/ComputeSharp.SourceGenerators/Models/ParameterInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ComputeSharp.SourceGenerators.Models;
-
-/// <summary>
-/// A model describing info on a constructor parameter.
-/// </summary>
-/// <param name="Type">The fully qualified type name.</param>
-/// <param name="Name">The parameter name.</param>
-internal sealed record ParameterInfo(string Type, string Name);
