bd1cb13e50dc4ff450b24bfca172602050e3b024
Sergio Pedri <sergio0694@live.com>
2022-04-16 12:51:04 +0200

    Add pixelate effect to unit tests
    This also includes a nested [AutoConstructor] buffer

--- tests/ComputeSharp.D2D1Interop.Tests/EndToEndTests.cs
+++ tests/ComputeSharp.D2D1Interop.Tests/EndToEndTests.cs
@@ -12,9 +12,15 @@ namespace ComputeSharp.D2D1Interop.Tests;
 public class EndToEndTests
 {
     [TestMethod]
-    public unsafe void InvertEffectWithCustomThreshold()
+    public unsafe void InvertWithCustomThreshold()
     {
         RunAndCompareShader(new InvertShader(1), "Landscape.png", "Landscape_Inverted.png");
+    }
+
+    [TestMethod]
+    public unsafe void Pixelate()
+    {
+        RunAndCompareShader(new PixelateShader(new PixelateShader.Constants(1280, 720, 16)), "Landscape.png", "Landscape_Pixelate.png");
     }
 
     /// <summary>
@@ -66,5 +72,48 @@ public partial struct InvertShader : ID2D1PixelShader
         float3 rgb = Hlsl.Saturate(this.number - color.RGB);
 
         return new(rgb, 1);
+    }
+}
+
+[D2DInputCount(1)]
+[D2DInputComplex(0)]
+[D2DRequiresScenePosition]
+[D2DEmbeddedBytecode(D2D1ShaderProfile.PixelShader40)]
+[AutoConstructor]
+public partial struct PixelateShader : ID2D1PixelShader
+{
+    [AutoConstructor]
+    public partial struct Constants
+    {
+        public readonly int inputWidth;
+        public readonly int inputHeight;
+        public readonly int cellSize;
+    }
+
+    private readonly Constants constants; 
+
+    public float4 Execute()
+    {
+        float2 scenePos = D2D1.GetScenePosition().XY;
+        uint x = (uint)Hlsl.Floor(scenePos.X);
+        uint y = (uint)Hlsl.Floor(scenePos.Y);
+
+        int cellX = (int)Hlsl.Floor(x / this.constants.cellSize);
+        int cellY = (int)Hlsl.Floor(y / this.constants.cellSize);
+
+        int x0 = cellX * this.constants.cellSize;
+        int y0 = cellY * this.constants.cellSize;
+
+        int x1 = Hlsl.Min(this.constants.inputWidth, x0 + this.constants.cellSize) - 1;
+        int y1 = Hlsl.Min(this.constants.inputHeight, y0 + this.constants.cellSize) - 1;
+
+        float4 sample0 = D2D1.SampleInputAtPosition(0, new int2(x0, y0));
+        float4 sample1 = D2D1.SampleInputAtPosition(0, new int2(x1, y0));
+        float4 sample2 = D2D1.SampleInputAtPosition(0, new int2(x0, y1));
+        float4 sample3 = D2D1.SampleInputAtPosition(0, new int2(x1, y1));
+
+        float4 color = (sample0 + sample1 + sample2 + sample3) / 4;
+
+        return color;
     }
 }
