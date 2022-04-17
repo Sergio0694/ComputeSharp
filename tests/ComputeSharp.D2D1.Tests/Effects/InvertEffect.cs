8a872832e14b1cac63dabe80e4b2cf7d7108ea84
Sergio Pedri <sergio0694@live.com>
2022-04-17 20:53:55 +0200

    Minor code refactoring

---
+++ tests/ComputeSharp.D2D1.Tests/Effects/InvertEffect.cs
@@ -0,0 +1,19 @@
+﻿namespace ComputeSharp.D2D1.Tests.Effects;
+
+[D2DInputCount(1)]
+[D2DInputSimple(0)]
+[D2DEmbeddedBytecode(D2D1ShaderProfile.PixelShader50)]
+[AutoConstructor]
+public partial struct InvertEffect : ID2D1PixelShader
+{
+    public float number;
+
+    /// <inheritdoc/>
+    public float4 Execute()
+    {
+        float4 color = D2D.GetInput(0);
+        float3 rgb = Hlsl.Saturate(this.number - color.RGB);
+
+        return new(rgb, 1);
+    }
+}
