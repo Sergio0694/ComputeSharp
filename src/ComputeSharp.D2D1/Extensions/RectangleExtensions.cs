ee231137a2de0d73fe8a5a8761fcb28eba2185ef
Sergio Pedri <sergio0694@live.com>
2022-04-17 03:18:58 +0200

    Add initial ID2D1DrawTransformMapper support and APIs

---
+++ src/ComputeSharp.D2D1/Extensions/RectangleExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Runtime.CompilerServices;
+using TerraFX.Interop.Windows;
+
+namespace ComputeSharp.D2D1.Extensions;
+
+/// <summary>
+/// Helper methods for <see cref="Rectangle"/>.
+/// </summary>
+internal static class RectangleExtensions
+{
+    /// <summary>
+    /// Creates a <see cref="RECT"/> from a <see cref="Rectangle"/> value.
+    /// </summary>
+    /// <param name="rectangle">The input <see cref="Rectangle"/> value.</param>
+    /// <returns>The resulting <see cref="RECT"/> value.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static RECT ToRECT(this in Rectangle rectangle)
+    {
+        RECT result;
+
+        result.left = rectangle.Left;
+        result.top = rectangle.Top;
+        result.right = rectangle.Right;
+        result.bottom = rectangle.Bottom;
+
+        return result;
+    }
+}
