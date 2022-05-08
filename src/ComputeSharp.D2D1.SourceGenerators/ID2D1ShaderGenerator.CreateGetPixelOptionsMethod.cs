b21193288195686ff2126c9e43abd65507f03e6e
Sergio Pedri <sergio0694@live.com>
2022-05-08 19:24:11 +0200

    Enable generation for ID2D1Shader.GetPixelOptions API

---
+++ src/ComputeSharp.D2D1.SourceGenerators/ID2D1ShaderGenerator.CreateGetPixelOptionsMethod.cs
@@ -0,0 +1,31 @@
+﻿using ComputeSharp.SourceGeneration.Extensions;
+using Microsoft.CodeAnalysis;
+
+namespace ComputeSharp.D2D1.SourceGenerators;
+
+/// <inheritdoc/>
+partial class ID2D1ShaderGenerator
+{
+    /// <summary>
+    /// A helper with all logic to generate the <c>GetPixelOptions</c> method.
+    /// </summary>
+    private static partial class GetPixelOptions
+    {
+        /// <summary>
+        /// Extracts the pixel options for the current shader.
+        /// </summary>
+        /// <param name="structDeclarationSymbol">The input <see cref="INamedTypeSymbol"/> instance to process.</param>
+        /// <param name="pixelOptions">The pixel options for the shader.</param>
+        public static void GetInfo(INamedTypeSymbol structDeclarationSymbol, out D2D1PixelOption pixelOptions)
+        {
+            if (structDeclarationSymbol.TryGetAttributeWithFullMetadataName("ComputeSharp.D2D1.D2DPixelOptionsAttribute", out AttributeData? attributeData))
+            {
+                pixelOptions = (D2D1PixelOption)attributeData!.ConstructorArguments[0].Value!;
+            }
+            else
+            {
+                pixelOptions = D2D1PixelOption.None;
+            }
+        }
+    }
+}
