0a86253dc7f9a7e3862d59376774bbd9706ccd7a
Sergio Pedri <sergio0694@live.com>
2022-06-04 17:12:49 +0200

    Switch ImageProcessing sample to top level statements

--- samples/ComputeSharp.ImageProcessing/Program.cs
+++ samples/ComputeSharp.ImageProcessing/Program.cs
@@ -7,40 +7,34 @@ using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Processing.Processors;
 using ImageSharpRgba32 = SixLabors.ImageSharp.PixelFormats.Rgba32;
 
-namespace ComputeSharp.ImageProcessing;
+Configuration.Default.PreferContiguousImageBuffers = true;
 
-class Program
+Console.WriteLine(">> Loading image");
+
+string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "city.jpg");
+using Image<ImageSharpRgba32> image = Image.Load<ImageSharpRgba32>(path);
+
+// Apply a series of processors and save the results
+foreach (var effect in new (string Name, IImageProcessor Processor)[]
 {
-    static void Main()
+    ("bokeh", new HlslBokehBlurProcessor(80, 2)),
+    ("gaussian", new HlslGaussianBlurProcessor(80))
+})
+{
+    Console.WriteLine($">> Applying {effect.Name}");
+
+    using Image copy = image.Clone(c => c.ApplyProcessor(effect.Processor));
+
+    Console.WriteLine($">> Saving {effect.Name} to disk");
+
+    string targetPath = Path.GetDirectoryName(path)!;
+
+    while (Path.GetFileName(targetPath) != "ComputeSharp.ImageProcessing")
     {
-        Configuration.Default.PreferContiguousImageBuffers = true;
-
-        Console.WriteLine(">> Loading image");
-        string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "city.jpg");
-        using Image<ImageSharpRgba32> image = Image.Load<ImageSharpRgba32>(path);
-
-        // Apply a series of processors and save the results
-        foreach (var effect in new (string Name, IImageProcessor Processor)[]
-        {
-            ("bokeh", new HlslBokehBlurProcessor(80, 2)),
-            ("gaussian", new HlslGaussianBlurProcessor(80))
-        })
-        {
-            Console.WriteLine($">> Applying {effect.Name}");
-            using Image copy = image.Clone(c => c.ApplyProcessor(effect.Processor));
-
-            Console.WriteLine($">> Saving {effect.Name} to disk");
-
-            string targetPath = Path.GetDirectoryName(path)!;
-
-            while (Path.GetFileName(targetPath) != "ComputeSharp.ImageProcessing")
-            {
-                targetPath = Path.GetDirectoryName(targetPath)!;
-            }
-
-            targetPath = Path.Combine(targetPath, $"{Path.GetFileNameWithoutExtension(path)}_{effect.Name}{Path.GetExtension(path)}");
-
-            copy.Save(targetPath);
-        }
+        targetPath = Path.GetDirectoryName(targetPath)!;
     }
+
+    targetPath = Path.Combine(targetPath, $"{Path.GetFileNameWithoutExtension(path)}_{effect.Name}{Path.GetExtension(path)}");
+
+    copy.Save(targetPath);
 }
