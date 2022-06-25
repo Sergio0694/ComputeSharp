3e1f18b6597c6ac2ddc076c5a78625ede3cc3673
Sergio Pedri <sergio0694@live.com>
2022-06-25 17:28:20 +0200

    Add ComputeSharp.Pix.NuGet test project

---
+++ tests/ComputeSharp.Pix.NuGet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using ComputeSharp;
+
+float[] array = Enumerable.Range(1, 100).Select(static i => (float)i).ToArray();
+
+// Create the graphics buffer
+using ReadWriteBuffer<float> gpuBuffer = GraphicsDevice.Default.AllocateReadWriteBuffer(array);
+
+// Run the shader
+using (var context = GraphicsDevice.Default.CreateComputeContext())
+{
+    context.Log("Dispatching shader");
+    context.BeginEvent("Dispatching event");
+    context.For(100, new MultiplyByTwo(gpuBuffer));
+    context.EndEvent();
+}
+
+// Get the data back
+float[] result = gpuBuffer.ToArray();
+
+// Validate results
+for (int i = 0; i < array.Length; i++)
+{
+    Trace.Assert(result[i] == array[i] * 2);
+}
+
+/// <summary>
+/// A sample kernel that is precompiled.
+/// </summary>
+[EmbeddedBytecode(DispatchAxis.X)]
+[AutoConstructor]
+internal readonly partial struct MultiplyByTwo : IComputeShader
+{
+    public readonly ReadWriteBuffer<float> buffer;
+
+    /// <inheritdoc/>
+    public void Execute()
+    {
+        buffer[ThreadIds.X] *= 2;
+    }
+}
