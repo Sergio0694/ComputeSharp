ecb899180701ddd1e6ea89636274cd8e80ddbb64
Sergio Pedri <sergio0694@live.com>
2022-08-08 00:38:21 +0200

    Fix names of D2D1 resource texture types

--- src/ComputeSharp.D2D1/Resources/D2D1ResourceTexture2D.cs
+++ src/ComputeSharp.D2D1/Resources/D2D1ResourceTexture2D.cs
@@ -7,19 +7,19 @@ namespace ComputeSharp.D2D1;
 /// This can be used from D2D1 pixel shaders to be able to pass arbitrary inputs to them.
 /// </summary>
 [AutoConstructorBehavior(AutoConstructorBehavior.IgnoreAndSetToDefault)]
-public readonly struct D2D1TextureResource2D
+public readonly struct D2D1ResourceTexture2D
 {
     /// <summary>
     /// Gets the width of the current texture.
     /// </summary>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public int Width => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource2D)}.{nameof(Width)}");
+    public int Width => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture2D)}.{nameof(Width)}");
 
     /// <summary>
     /// Gets the height of the current texture.
     /// </summary>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public int Height => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource2D)}.{nameof(Height)}");
+    public int Height => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture2D)}.{nameof(Height)}");
 
     /// <summary>
     /// Gets a single <see cref="Float4"/> value from the current readonly texture.
@@ -27,14 +27,14 @@ public readonly struct D2D1TextureResource2D
     /// <param name="x">The horizontal offset of the value to get.</param>
     /// <param name="y">The vertical offset of the value to get.</param>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public ref readonly Float4 this[int x, int y] => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource2D)}[{typeof(int)}, {typeof(int)}]");
+    public ref readonly Float4 this[int x, int y] => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture2D)}[{typeof(int)}, {typeof(int)}]");
 
     /// <summary>
     /// Gets a single <see cref="Float4"/> value from the current readonly texture.
     /// </summary>
     /// <param name="xy">The coordinates of the value to get.</param>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public ref readonly Float4 this[Int2 xy] => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource2D)}[{typeof(Int2)}]");
+    public ref readonly Float4 this[Int2 xy] => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture2D)}[{typeof(Int2)}]");
 
     /// <summary>
     /// Gets a single <see cref="Float4"/> value from the current readonly texture with linear sampling.
@@ -42,12 +42,12 @@ public readonly struct D2D1TextureResource2D
     /// <param name="u">The horizontal normalized offset of the value to get.</param>
     /// <param name="v">The vertical normalized offset of the value to get.</param>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public ref readonly Float4 this[float u, float v] => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource2D)}[{typeof(float)}, {typeof(float)}]");
+    public ref readonly Float4 this[float u, float v] => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture2D)}[{typeof(float)}, {typeof(float)}]");
 
     /// <summary>
     /// Gets a single <see cref="Float4"/> value from the current readonly texture with linear sampling.
     /// </summary>
     /// <param name="uv">The normalized coordinates of the value to get.</param>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public ref readonly Float4 this[Float2 uv] => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource2D)}[{typeof(Float2)}]");
+    public ref readonly Float4 this[Float2 uv] => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture2D)}[{typeof(Float2)}]");
 }
--- src/ComputeSharp.D2D1/Resources/D2D1ResourceTexture3D.cs
+++ src/ComputeSharp.D2D1/Resources/D2D1ResourceTexture3D.cs
@@ -7,25 +7,25 @@ namespace ComputeSharp.D2D1;
 /// This can be used from D2D1 pixel shaders to be able to pass arbitrary inputs to them.
 /// </summary>
 [AutoConstructorBehavior(AutoConstructorBehavior.IgnoreAndSetToDefault)]
-public readonly struct D2D1TextureResource3D
+public readonly struct D2D1ResourceTexture3D
 {
     /// <summary>
     /// Gets the width of the current texture.
     /// </summary>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public int Width => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource3D)}.{nameof(Width)}");
+    public int Width => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture3D)}.{nameof(Width)}");
 
     /// <summary>
     /// Gets the height of the current texture.
     /// </summary>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public int Height => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource3D)}.{nameof(Height)}");
+    public int Height => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture3D)}.{nameof(Height)}");
 
     /// <summary>
     /// Gets the depth of the current texture.
     /// </summary>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public int Depth => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource3D)}.{nameof(Depth)}");
+    public int Depth => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture3D)}.{nameof(Depth)}");
 
     /// <summary>
     /// Gets a single <see cref="Float4"/> value from the current readonly texture.
@@ -34,14 +34,14 @@ public readonly struct D2D1TextureResource3D
     /// <param name="y">The vertical offset of the value to get.</param>
     /// <param name="z">The depthwise offset of the value to get.</param>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public ref readonly Float4 this[int x, int y, int z] => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource3D)}[{typeof(int)}, {typeof(int)}, {typeof(int)}]");
+    public ref readonly Float4 this[int x, int y, int z] => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture3D)}[{typeof(int)}, {typeof(int)}, {typeof(int)}]");
 
     /// <summary>
     /// Gets a single <see cref="Float4"/> value from the current readonly texture.
     /// </summary>
     /// <param name="xyz">The coordinates of the value to get.</param>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public ref readonly Float4 this[Int3 xyz] => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource3D)}[{typeof(Int3)}]");
+    public ref readonly Float4 this[Int3 xyz] => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture3D)}[{typeof(Int3)}]");
 
     /// <summary>
     /// Gets a single <see cref="Float4"/> value from the current readonly texture with linear sampling.
@@ -50,12 +50,12 @@ public readonly struct D2D1TextureResource3D
     /// <param name="v">The vertical normalized offset of the value to get.</param>
     /// <param name="w">The depthwise offset of the value to get.</param>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public ref readonly Float4 this[float u, float v, float w] => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource3D)}[{typeof(float)}, {typeof(float)}, {typeof(float)}]");
+    public ref readonly Float4 this[float u, float v, float w] => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture3D)}[{typeof(float)}, {typeof(float)}, {typeof(float)}]");
 
     /// <summary>
     /// Gets a single <see cref="Float4"/> value from the current readonly texture with linear sampling.
     /// </summary>
     /// <param name="uvw">The normalized coordinates of the value to get.</param>
     /// <remarks>This API can only be used from a compute shader, and will always throw if used anywhere else.</remarks>
-    public ref readonly Float4 this[Float3 uvw] => throw new InvalidExecutionContextException($"{typeof(D2D1TextureResource3D)}[{typeof(Float3)}]");
+    public ref readonly Float4 this[Float3 uvw] => throw new InvalidExecutionContextException($"{typeof(D2D1ResourceTexture3D)}[{typeof(Float3)}]");
 }
