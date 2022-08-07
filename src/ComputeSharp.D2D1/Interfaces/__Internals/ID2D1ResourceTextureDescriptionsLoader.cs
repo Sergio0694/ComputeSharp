abb1d46a2d5f3289c568106e69f2c4ff54e3cab5
Sergio Pedri <sergio0694@live.com>
2022-08-07 23:39:38 +0200

    Add ID2D1ResourceTextureDescriptionsLoader and API

---
+++ src/ComputeSharp.D2D1/Interfaces/__Internals/ID2D1ResourceTextureDescriptionsLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ComputeSharp.D2D1.__Internals;
+
+/// <summary>
+/// A base <see langword="interface"/> representing a loader for resource texture descriptions in a D2D1 shader.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+[Obsolete("This interface is not intended to be used directly by user code")]
+public interface ID2D1ResourceTextureDescriptionsLoader
+{
+    /// <summary>
+    /// Loads the available resource texture descriptions for the shader.
+    /// </summary>
+    /// <param name="data">The sequence of serialized resource texture descriptions.</param>
+    void LoadResourceTextureDescriptions(ReadOnlySpan<byte> data);
+}
