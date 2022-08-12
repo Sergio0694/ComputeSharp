47dfbf471852d517d9d12443b33854313743c2e0
Sergio Pedri <sergio0694@live.com>
2022-08-12 02:33:49 +0200

    Add end to end test with 2D resource texture

--- tests/ComputeSharp.D2D1.Tests/D2D1ResourceTextureManagerTests.cs
+++ tests/ComputeSharp.D2D1.Tests/D2D1ResourceTextureManagerTests.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using ComputeSharp.D2D1.Interop;
+using ComputeSharp.D2D1.Tests.Helpers;
+using ComputeSharp.Tests.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using TerraFX.Interop.DirectX;
 using TerraFX.Interop.Windows;
+
+#pragma warning disable CS0649
 
 namespace ComputeSharp.D2D1.Tests;
 
 [TestClass]
 [TestCategory("D2D1ResourceTextureManager")]
-public class D2D1ResourceTextureManagerTests
+public partial class D2D1ResourceTextureManagerTests
 {
     [TestMethod]
     public unsafe void VerifyInterfaces()
@@ -47,5 +57,74 @@ public class D2D1ResourceTextureManagerTests
         Assert.AreEqual(E.E_NOINTERFACE, (int)resourceTextureManager.AsIID(&uuidOfGarbage, &garbage));
 
         Assert.IsTrue(garbage.Get() is null);
+    }
+
+    [TestMethod]
+    public unsafe void LoadPixelsFromResourceTexture2D()
+    {
+        using ComPtr<ID2D1Factory2> d2D1Factory2 = D2D1Helper.CreateD2D1Factory2();
+        using ComPtr<ID2D1Device> d2D1Device = D2D1Helper.CreateD2D1Device(d2D1Factory2.Get());
+        using ComPtr<ID2D1DeviceContext> d2D1DeviceContext = D2D1Helper.CreateD2D1DeviceContext(d2D1Device.Get());
+
+        D2D1PixelShaderEffect.RegisterForD2D1Factory1<SampleShader>(d2D1Factory2.Get(), null, out _);
+
+        using ComPtr<ID2D1Effect> d2D1Effect = default;
+
+        D2D1PixelShaderEffect.CreateFromD2D1DeviceContext<SampleShader>(d2D1DeviceContext.Get(), (void**)d2D1Effect.GetAddressOf());
+
+        D2D1PixelShaderEffect.SetConstantBufferForD2D1Effect(default(SampleShader), d2D1Effect.Get());
+
+        using ComPtr<IUnknown> resourceTextureManager = default;
+
+        D2D1ResourceTextureManager.Create((void**)resourceTextureManager.GetAddressOf());
+
+        D2D1PixelShaderEffect.SetResourceTextureManagerForD2D1Effect(d2D1Effect.Get(), resourceTextureManager.Get(), 0);
+
+        string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+        string expectedPath = Path.Combine(assemblyPath, "Assets", "Landscape.png");
+        string destinationPath = Path.Combine(assemblyPath, "temp", "IndexedFromResourceTexture2D.png");
+
+        using Image<Rgba32> expected = Image.Load<Rgba32>(expectedPath);
+
+        if (!expected.DangerousTryGetSinglePixelMemory(out Memory<Rgba32> pixels))
+        {
+            Assert.Inconclusive();
+        }
+
+        D2D1ResourceTextureManager.Initialize(
+            resourceTextureManager: resourceTextureManager.Get(),
+            resourceId: Guid.NewGuid(),
+            extents: stackalloc[] { (uint)expected.Width, (uint)expected.Height },
+            bufferPrecision: D2D1BufferPrecision.UInt8Normalized,
+            channelDepth: D2D1ChannelDepth.Four,
+            filter: D2D1Filter.MinMagMipPoint,
+            extendModes: stackalloc[] { D2D1ExtendMode.Clamp, D2D1ExtendMode.Clamp },
+            data: MemoryMarshal.AsBytes(pixels.Span),
+            strides: stackalloc[] { (uint)(sizeof(Rgba32) * expected.Width) });
+
+        using ComPtr<ID2D1Bitmap> d2D1BitmapTarget = D2D1Helper.CreateD2D1BitmapAndSetAsTarget(d2D1DeviceContext.Get(), (uint)expected.Width, (uint)expected.Height);
+
+        D2D1Helper.DrawEffect(d2D1DeviceContext.Get(), d2D1Effect.Get());
+
+        using ComPtr<ID2D1Bitmap1> d2D1Bitmap1Buffer = D2D1Helper.CreateD2D1Bitmap1Buffer(d2D1DeviceContext.Get(), d2D1BitmapTarget.Get(), out D2D1_MAPPED_RECT d2D1MappedRect);
+
+        WICHelper.SaveBitmapToFile(destinationPath, (uint)expected.Width, (uint)expected.Height, d2D1MappedRect.pitch, d2D1MappedRect.bits);
+
+        TolerantImageComparer.AssertEqual(destinationPath, expectedPath, 0.00001f);
+    }
+
+    [D2DInputCount(0)]
+    [D2DRequiresScenePosition]
+    private partial struct SampleShader : ID2D1PixelShader
+    {
+        [D2DResourceTextureIndex(0)]
+        private D2D1ResourceTexture2D<float4> source;
+
+        public float4 Execute()
+        {
+            int2 xy = (int2)D2D.GetScenePosition().XY;
+
+            return source[xy];
+        }
     }
 }
