0b6d2b0ddf14e8d63fb4ad4c3586043fa86b3c8e
Sergio Pedri <sergio0694@live.com>
2022-08-29 00:58:59 +0200

    Add readback and upload transfer texture 1D types

---
+++ src/ComputeSharp/Graphics/Resources/Debug/TransferTexture1DDebugView{T}.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace ComputeSharp.Resources.Debug;
+
+/// <summary>
+/// A debug proxy used to display items in a <see cref="TransferTexture1D{T}"/> instance.
+/// </summary>
+/// <typeparam name="T">The type of items to display.</typeparam>
+internal sealed class TransferTexture1DDebugView<T>
+    where T : unmanaged
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TransferTexture1DDebugView{T}"/> class with the specified parameters.
+    /// </summary>
+    /// <param name="texture">The input <see cref="TransferTexture1D{T}"/> instance with the items to display.</param>
+    public TransferTexture1DDebugView(TransferTexture1D<T>? texture)
+    {
+        Items = texture?.Span.ToArray();
+    }
+
+    /// <summary>
+    /// Gets the items to display for the current instance.
+    /// </summary>
+    [DebuggerBrowsable(DebuggerBrowsableState.Collapsed)]
+    public T[]? Items { get; }
+}
---
+++ src/ComputeSharp/Graphics/Resources/UploadTexture1D{T}.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using ComputeSharp.Graphics.Resources.Enums;
+using ComputeSharp.Resources;
+using ComputeSharp.Resources.Debug;
+
+namespace ComputeSharp;
+
+/// <summary>
+/// A <see langword="class"/> representing a typed 1D texture stored on CPU memory, that can be used to transfer data to the GPU.
+/// </summary>
+/// <typeparam name="T">The type of items stored on the texture.</typeparam>
+[DebuggerTypeProxy(typeof(TransferTexture1DDebugView<>))]
+[DebuggerDisplay("{ToString(),raw}")]
+public sealed class UploadTexture1D<T> : TransferTexture1D<T>
+    where T : unmanaged
+{
+    /// <summary>
+    /// Creates a new <see cref="UploadTexture1D{T}"/> instance with the specified parameters.
+    /// </summary>
+    /// <param name="device">The <see cref="GraphicsDevice"/> associated with the current instance.</param>
+    /// <param name="width">The width of the texture.</param>
+    /// <param name="allocationMode">The allocation mode to use for the new resource.</param>
+    internal UploadTexture1D(GraphicsDevice device, int width, AllocationMode allocationMode)
+        : base(device, width, ResourceType.Upload, allocationMode)
+    {
+    }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        return $"ComputeSharp.UploadTexture1D<{typeof(T)}>[{Width}]";
+    }
+}
