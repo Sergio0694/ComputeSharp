c2bd62d86eee75433e1c3df4f30b73e439e717f2
Sergio Pedri <sergio0694@live.com>
2022-09-04 15:32:11 +0200

    Add global shader method test

--- tests/ComputeSharp.Tests.GlobalStatements/Program.cs
+++ tests/ComputeSharp.Tests.GlobalStatements/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ComputeSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+float[] numbers = Enumerable.Range(0, 128).Select(i => (float)i).ToArray();
+
+using GraphicsDevice device = GraphicsDevice.GetDefault();
+using ReadWriteBuffer<float> buffer = device.AllocateReadWriteBuffer(numbers);
+
+device.For(buffer.Length, new ApplyFunctionShader(buffer, AddHalf));
+
+foreach (ref float number in numbers.AsSpan())
+{
+    number = AddHalf(number);
+}
+
+float[] result = buffer.ToArray();
+
+CollectionAssert.AreEqual(
+    expected: numbers,
+    actual: result);
+
+Console.WriteLine("Test passed successfully");
+
+// See https://github.com/Sergio0694/ComputeSharp/issues/374
+[ShaderMethod]
+static float AddHalf(float input) => input + 0.5f;
+
+[AutoConstructor]
+public readonly partial struct ApplyFunctionShader : IComputeShader
+{
+    private readonly ReadWriteBuffer<float> sourceTexture;
+    private readonly Func<float, float> func;
+
+    public void Execute()
+    {
+        this.sourceTexture[ThreadIds.X] = this.func(this.sourceTexture[ThreadIds.X]);
+    }
+}
