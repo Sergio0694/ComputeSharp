983e874f4dae747b4e06f4125bbb548165a98275
Sergio Pedri <sergio0694@live.com>
2022-10-07 20:38:33 +0200

    Add [SkipLocalsInit] to all projects

--- src/ComputeSharp.Core/Properties/AssemblyInfo.cs
+++ src/ComputeSharp.Core/Properties/AssemblyInfo.cs
@@ -4,6 +4,7 @@
 
 [assembly: InternalsVisibleTo("ComputeSharp")]
 [assembly: InternalsVisibleTo("ComputeSharp.Dynamic")]
+[assembly: InternalsVisibleTo("ComputeSharp.Pix")]
 [assembly: InternalsVisibleTo("ComputeSharp.Uwp")]
 [assembly: InternalsVisibleTo("ComputeSharp.WinUI")]
 [assembly: InternalsVisibleTo("ComputeSharp.D2D1")] 
--- src/ComputeSharp.Pix/Properties/AssemblyInfo.cs
+++ src/ComputeSharp.Pix/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
-﻿using System.Runtime.Versioning;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+[module: SkipLocalsInit]
 
 [assembly: SupportedOSPlatform("windows6.2")]
