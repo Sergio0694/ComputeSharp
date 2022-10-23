9bf5a058524e5a6c6984810fef033968e476a669
Sergio Pedri <sergio0694@live.com>
2022-10-23 20:44:11 +0200

    Add reference tracking support to PixelShaderEffect<T>

---
+++ src/ComputeSharp.D2D1.Uwp/PixelShaderEffect{T}.ReferenceTracking.cs
@@ -0,0 +1,52 @@
+using System;
+using ComputeSharp.Interop;
+using System.Runtime.CompilerServices;
+
+namespace ComputeSharp.D2D1.Uwp;
+
+/// <inheritdoc/>
+partial class PixelShaderEffect<T>
+{
+    /// <summary>
+    /// Creates a new <see cref="PixelShaderEffect{T}"/> instance.
+    /// </summary>
+    public PixelShaderEffect()
+    {
+        this.referenceTracker = new();
+    }
+
+    /// <summary>
+    /// Releases unmanaged resources and performs other cleanup operations.
+    /// </summary>
+    ~PixelShaderEffect()
+    {
+        this.referenceTracker.Dispose();
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        this.referenceTracker.Dispose();
+
+        GC.SuppressFinalize(this);
+    }
+
+    /// <inheritdoc cref="IReferenceTrackedObject.GetReferenceTracker"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal ref ReferenceTracker GetReferenceTracker()
+    {
+        return ref this.referenceTracker;
+    }
+
+    /// <inheritdoc/>
+    ref ReferenceTracker IReferenceTrackedObject.GetReferenceTracker()
+    {
+        return ref this.referenceTracker;
+    }
+
+    /// <inheritdoc/>
+    void IReferenceTrackedObject.DangerousOnDispose()
+    {
+        // TODO
+    }
+}
