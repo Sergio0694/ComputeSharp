b6f739ca9daa28635cfe768d7eeb83477d3e0083
Sergio Pedri <sergio0694@live.com>
2022-10-25 00:41:46 +0200

    Implement ICanvasImage public APIs

--- src/ComputeSharp.D2D1.Uwp/PixelShaderEffect{T}.ICanvasImage.cs
+++ src/ComputeSharp.D2D1.Uwp/PixelShaderEffect{T}.ICanvasImage.cs
@@ -1,22 +1,58 @@
-using System;
 using System.Numerics;
+using System.Runtime.InteropServices;
+using ABI.Microsoft.Graphics.Canvas;
+using ComputeSharp.D2D1.Extensions;
+using ComputeSharp.Interop;
 using Microsoft.Graphics.Canvas;
+using TerraFX.Interop.Windows;
 using Windows.Foundation;
+using ICanvasResourceCreator = Microsoft.Graphics.Canvas.ICanvasResourceCreator;
 
 namespace ComputeSharp.D2D1.Uwp;
 
 /// <inheritdoc/>
-partial class PixelShaderEffect<T>
+unsafe partial class PixelShaderEffect<T>
 {
     /// <inheritdoc/>
     Rect ICanvasImage.GetBounds(ICanvasResourceCreator resourceCreator)
     {
-        throw new NotSupportedException();
+        return GetBounds(resourceCreator, null);
     }
 
     /// <inheritdoc/>
     Rect ICanvasImage.GetBounds(ICanvasResourceCreator resourceCreator, Matrix3x2 transform)
     {
-        throw new NotSupportedException();
+        return GetBounds(resourceCreator, &transform);
+    }
+
+    /// <inheritdoc cref="ICanvasImage.GetBounds(ICanvasResourceCreator, Matrix3x2)"/>
+    private Rect GetBounds(ICanvasResourceCreator resourceCreator, Matrix3x2* transform)
+    {
+        using ReferenceTracker.Lease _0 = GetReferenceTracker().GetLease();
+
+        using ComPtr<IUnknown> canvasResourceCreatorUnknown = default;
+        using ComPtr<IUnknown> canvasImageInteropUnknown = default;
+
+        // Get the IUnknown* references for the ICanvasResourceCreator and ICanvasImageInterop objects
+        canvasResourceCreatorUnknown.Attach((IUnknown*)Marshal.GetIUnknownForObject(resourceCreator));
+        canvasImageInteropUnknown.Attach((IUnknown*)Marshal.GetIUnknownForObject(this));
+
+        using ComPtr<ABI.Microsoft.Graphics.Canvas.ICanvasResourceCreator> canvasResourceCreator = default;
+        using ComPtr<ICanvasImageInterop> canvasImageInterop = default;
+
+        // Get the ICanvasResourceCreator* and ICanvasImageInterop* pointers
+        canvasResourceCreatorUnknown.CopyTo(canvasResourceCreator.GetAddressOf()).Assert();
+        canvasImageInteropUnknown.CopyTo(canvasImageInterop.GetAddressOf()).Assert();
+
+        Rect bounds;
+
+        // Forward the actual logic to Win2D to compute the image bounds (it needs the internal context)
+        Win2D.GetBoundsForICanvasImageInterop(
+            resourceCreator: canvasResourceCreator.Get(),
+            image: canvasImageInterop.Get(),
+            transform: transform,
+            rect: &bounds).Assert();
+
+        return bounds;
     }
 }
