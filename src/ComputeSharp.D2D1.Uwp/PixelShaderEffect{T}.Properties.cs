e2550a2b548a50a433a94f641f39de95269b52c8
Sergio Pedri <sergio0694@live.com>
2022-12-02 15:12:54 +0100

    Add fixed buffer type for SourceCollection

--- src/ComputeSharp.D2D1.Uwp/PixelShaderEffect{T}.Properties.cs
+++ src/ComputeSharp.D2D1.Uwp/PixelShaderEffect{T}.Properties.cs
@@ -2,7 +2,6 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using ComputeSharp.D2D1.Interop;
 using Microsoft.Graphics.Canvas;
-using Windows.Graphics.Effects;
 
 namespace ComputeSharp.D2D1.Uwp;
 
@@ -57,36 +56,4 @@ partial class PixelShaderEffect<T>
     /// Gets the <see cref="ResourceTextureManagerCollection"/> object associated with the current instance.
     /// </summary>
     public ResourceTextureManagerCollection ResourceTextureManagers { get; }
-
-    /// <summary>
-    /// Represents the collection of <see cref="IGraphicsEffectSource"/> sources in a <see cref="PixelShaderEffect{T}"/> instance.
-    /// </summary>
-    public sealed class SourceCollection
-    {
-        /// <summary>
-        /// Creates a new <see cref="SourceCollection"/> instance with the specified parameters.
-        /// </summary>
-        /// <param name="owner">The owning <see cref="PixelShaderEffect{T}"/> instance.</param>
-        internal SourceCollection(PixelShaderEffect<T> owner)
-        {
-            Owner = owner;
-        }
-
-        /// <summary>
-        /// The owning <see cref="PixelShaderEffect{T}"/> instance.
-        /// </summary>
-        internal PixelShaderEffect<T> Owner { get; }
-
-        /// <summary>
-        /// Gets or sets the <see cref="IGraphicsEffectSource"/> source at a specified index.
-        /// </summary>
-        /// <param name="index">The index of the <see cref="IGraphicsEffectSource"/> source to get or set.</param>
-        /// <returns>The <see cref="IGraphicsEffectSource"/> source at the specified index.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="index"/> is not a valid index for the current effect.</exception>
-        public IGraphicsEffectSource this[int index]
-        {
-            get => null!;
-            set { }
-        }
-    }
 }
