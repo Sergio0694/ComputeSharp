8cbb3c958eb304c242e915675ee36531b209856a
Sergio Pedri <sergio0694@live.com>
2023-09-16 22:55:49 +0200

    Add InvalidEffectIdValueAnalyzer

--- src/ComputeSharp.SourceGeneration/Extensions/AttributeDataExtensions.cs
+++ src/ComputeSharp.SourceGeneration/Extensions/AttributeDataExtensions.cs
@@ -9,6 +9,21 @@ namespace ComputeSharp.SourceGeneration.Extensions;
 internal static class AttributeDataExtensions
 {
     /// <summary>
+    /// Tries to get the location of the input <see cref="AttributeData"/> instance.
+    /// </summary>
+    /// <param name="attributeData">The input <see cref="AttributeData"/> instance to get the location for.</param>
+    /// <returns>The resulting location for <paramref name="attributeData"/>, if a syntax reference is available.</returns>
+    public static Location? GetLocation(this AttributeData attributeData)
+    {
+        if (attributeData.ApplicationSyntaxReference is { } syntaxReference)
+        {
+            return syntaxReference.SyntaxTree.GetLocation(syntaxReference.Span);
+        }
+
+        return null;
+    }
+
+    /// <summary>
     /// Tries to get a constructor argument at a given index from the input <see cref="AttributeData"/> instance.
     /// </summary>
     /// <typeparam name="T">The type of constructor argument to retrieve.</typeparam>
