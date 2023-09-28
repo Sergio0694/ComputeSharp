856475d9f8aefb2b06d0e7d2c3adf2cf452863de
Sergio Pedri <sergio0694@live.com>
2023-09-28 02:19:30 +0200

    Remove two unused methods
    Note clear why this wasn't failing the build before

--- samples/ComputeSharp.SwapChain.Shaders.Shared/TriangleGridContouring.cs
+++ samples/ComputeSharp.SwapChain.Shaders.Shared/TriangleGridContouring.cs
@@ -25,12 +25,6 @@ internal readonly partial struct TriangleGridContouring : IPixelShader<float4>
         return new(c, -s, s, c);
     }
 
-    // IQ's float2 to float hash.
-    private static float Hash21(float2 p)
-    {
-        return Hlsl.Frac(Hlsl.Sin(Hlsl.Dot(p, new float2(27.619f, 57.583f))) * 43758.5453f);
-    }
-
     // float2 to float2 hash.
     private float2 Hash22(float2 p)
     {
