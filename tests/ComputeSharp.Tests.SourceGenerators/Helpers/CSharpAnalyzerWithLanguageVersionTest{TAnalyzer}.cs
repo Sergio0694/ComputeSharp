d076ce5c054a2e6b9ab91fa76633c40380517d26
Sergio Pedri <sergio0694@live.com>
2023-10-19 11:52:31 +0200

    Add test for new accessibility analyzer

---
+++ tests/ComputeSharp.Tests.SourceGenerators/Helpers/CSharpAnalyzerWithLanguageVersionTest{TAnalyzer}.cs
@@ -0,0 +1,55 @@
+extern alias Core;
+extern alias D3D12;
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis;
+
+namespace ComputeSharp.Tests.SourceGenerators.Helpers;
+
+/// <summary>
+/// A custom <see cref="CSharpAnalyzerTest{TAnalyzer, TVerifier}"/> that uses a specific C# language version to parse code.
+/// </summary>
+/// <typeparam name="TAnalyzer">The type of the analyzer to test.</typeparam>
+internal sealed class CSharpAnalyzerWithLanguageVersionTest<TAnalyzer> : CSharpAnalyzerTest<TAnalyzer, MSTestVerifier>
+    where TAnalyzer : DiagnosticAnalyzer, new()
+{
+    /// <summary>
+    /// The C# language version to use to parse code.
+    /// </summary>
+    private readonly LanguageVersion languageVersion;
+
+    /// <summary>
+    /// Creates a new <see cref="CSharpAnalyzerWithLanguageVersionTest{TAnalyzer}"/> instance with the specified paramaters.
+    /// </summary>
+    /// <param name="languageVersion">The C# language version to use to parse code.</param>
+    private CSharpAnalyzerWithLanguageVersionTest(LanguageVersion languageVersion)
+    {
+        this.languageVersion = languageVersion;
+    }
+
+    /// <inheritdoc/>
+    protected override ParseOptions CreateParseOptions()
+    {
+        return new CSharpParseOptions(this.languageVersion, DocumentationMode.Diagnose);
+    }
+
+    /// <inheritdoc cref="AnalyzerVerifier{TAnalyzer, TTest, TVerifier}.VerifyAnalyzerAsync"/>
+    /// <param name="source">The source code to analyze.</param>
+    /// <param name="languageVersion">The language version to use to run the test.</param>
+    public static Task VerifyAnalyzerAsync(string source, LanguageVersion languageVersion = LanguageVersion.CSharp11)
+    {
+        CSharpAnalyzerWithLanguageVersionTest<TAnalyzer> test = new(languageVersion) { TestCode = source };
+
+        test.TestState.ReferenceAssemblies = ReferenceAssemblies.Net.Net70;
+        test.TestState.AdditionalReferences.Add(MetadataReference.CreateFromFile(typeof(Core::ComputeSharp.Hlsl).Assembly.Location));
+        test.TestState.AdditionalReferences.Add(MetadataReference.CreateFromFile(typeof(D3D12::ComputeSharp.IComputeShader).Assembly.Location));
+
+        return test.RunAsync(CancellationToken.None);
+    }
+}
