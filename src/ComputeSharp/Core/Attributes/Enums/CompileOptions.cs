18e9a5ac72e68d246d0d09b52b8bbb6dddb04cf3
Sergio Pedri <sergio0694@live.com>
2023-10-27 13:14:55 +0200

    Add unit test for shader with no reflection metadata

--- src/ComputeSharp/Core/Attributes/Enums/CompileOptions.cs
+++ src/ComputeSharp/Core/Attributes/Enums/CompileOptions.cs
@@ -131,8 +131,8 @@ public enum CompileOptions
     ResourcesMayAlias = 1 << 13,
 
     /// <summary>
-    /// Strips the reflection data from the generated shader bytecode. The bytecode size will be significantly lower,
-    /// but trying to perform reflection on the shader will fail at runtime. Recommend if not reflection is done.
+    /// Strips the reflection data from the generated shader bytecode. The bytecode size will be significantly lower, but
+    /// trying to perform reflection on the shader will return inaccurate results. Recommend if not reflection is done.
     /// </summary>
     /// <remarks>
     /// This flag maps to <c>-Qstrip_reflect</c>.
