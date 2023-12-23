18e8f03cb1a63be434a3432128ab61732ae4950b
Sergio Pedri <sergio0694@live.com>
2023-12-23 17:32:34 +0100

    Use CSharpGeneratorTest in output tests

--- tests/ComputeSharp.Tests.SourceGenerators/Helpers/CSharpGeneratorTest{TGenerator}.cs
+++ tests/ComputeSharp.Tests.SourceGenerators/Helpers/CSharpGeneratorTest{TGenerator}.cs
@@ -6,8 +6,10 @@ extern alias D2D1;
 extern alias D3D12;
 #endif
 
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
 using Basic.Reference.Assemblies;
 using Microsoft.CodeAnalysis.CSharp;
@@ -30,6 +32,54 @@ internal static class CSharpGeneratorTest<TGenerator>
     /// <param name="diagnosticsIds">The expected diagnostics ids to be generated.</param>
     public static void VerifyDiagnostics(string source, params string[] diagnosticsIds)
     {
+        RunGenerator(source, out Compilation compilation, out ImmutableArray<Diagnostic> diagnostics);
+
+        Dictionary<string, Diagnostic> diagnosticMap = diagnostics.DistinctBy(diagnostic => diagnostic.Id).ToDictionary(diagnostic => diagnostic.Id);
+
+        // Check that the diagnostics match
+        Assert.IsTrue(diagnosticMap.Keys.ToHashSet().SetEquals(diagnosticsIds), $"Diagnostics didn't match. {string.Join(", ", diagnosticMap.Values)}");
+
+        // If the compilation was supposed to succeed, ensure that no further errors were generated
+        if (diagnosticsIds.Length == 0)
+        {
+            // Compute diagnostics for the final compiled output (just include errors)
+            List<Diagnostic> outputCompilationDiagnostics = compilation.GetDiagnostics().Where(diagnostic => diagnostic.Severity == DiagnosticSeverity.Error).ToList();
+
+            Assert.IsTrue(outputCompilationDiagnostics.Count == 0, $"resultingIds: {string.Join(", ", outputCompilationDiagnostics)}");
+        }
+    }
+
+    /// <summary>
+    /// Verifies the resulting sources produced by a source generator.
+    /// </summary>
+    /// <param name="source">The input source to process.</param>
+    /// <param name="result">The expected source to be generated.</param>
+    public static void VerifySources(string source, (string Filename, string Source) result)
+    {
+        RunGenerator(source, out Compilation compilation, out ImmutableArray<Diagnostic> diagnostics);
+
+        // Ensure that no diagnostics were generated
+        CollectionAssert.AreEquivalent(Array.Empty<Diagnostic>(), diagnostics);
+
+        // Update the assembly version using the version from the assembly of the input generators.
+        // This allows the tests to not need updates whenever the version of the MVVM Toolkit changes.
+        string expectedText = result.Source.Replace("<ASSEMBLY_VERSION>", $"\"{typeof(TGenerator).Assembly.GetName().Version}\"");
+        string actualText = compilation.SyntaxTrees.Single(tree => Path.GetFileName(tree.FilePath) == result.Filename).ToString();
+
+        Assert.AreEqual(expectedText, actualText);
+    }
+
+    /// <summary>
+    /// Runs a generator and gathers the output results.
+    /// </summary>
+    /// <param name="source">The input source to process.</param>
+    /// <param name="compilation"><inheritdoc cref="GeneratorDriver.RunGeneratorsAndUpdateCompilation" path="/param[@name='outputCompilation']/node()"/></param>
+    /// <param name="diagnostics"><inheritdoc cref="GeneratorDriver.RunGeneratorsAndUpdateCompilation" path="/param[@name='diagnostics']/node()"/></param>
+    private static void RunGenerator(
+        string source,
+        out Compilation compilation,
+        out ImmutableArray<Diagnostic> diagnostics)
+    {
         // Get all assembly references for the .NET TFM and ComputeSharp
         IEnumerable<MetadataReference> metadataReferences =
         [
@@ -48,7 +98,7 @@ internal static class CSharpGeneratorTest<TGenerator>
             CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp12));
 
         // Create the original compilation
-        CSharpCompilation compilation = CSharpCompilation.Create(
+        CSharpCompilation originalCompilation = CSharpCompilation.Create(
             "original",
             [sourceTree],
             metadataReferences,
@@ -58,20 +108,6 @@ internal static class CSharpGeneratorTest<TGenerator>
         GeneratorDriver driver = CSharpGeneratorDriver.Create(new TGenerator()).WithUpdatedParseOptions((CSharpParseOptions)sourceTree.Options);
 
         // Run all source generators on the input source code
-        _ = driver.RunGeneratorsAndUpdateCompilation(compilation, out Compilation outputCompilation, out ImmutableArray<Diagnostic> diagnostics);
-
-        Dictionary<string, Diagnostic> diagnosticMap = diagnostics.DistinctBy(diagnostic => diagnostic.Id).ToDictionary(diagnostic => diagnostic.Id);
-
-        // Check that the diagnostics match
-        Assert.IsTrue(diagnosticMap.Keys.ToHashSet().SetEquals(diagnosticsIds), $"Diagnostics didn't match. {string.Join(", ", diagnosticMap.Values)}");
-
-        // If the compilation was supposed to succeed, ensure that no further errors were generated
-        if (diagnosticsIds.Length == 0)
-        {
-            // Compute diagnostics for the final compiled output (just include errors)
-            List<Diagnostic> outputCompilationDiagnostics = outputCompilation.GetDiagnostics().Where(diagnostic => diagnostic.Severity == DiagnosticSeverity.Error).ToList();
-
-            Assert.IsTrue(outputCompilationDiagnostics.Count == 0, $"resultingIds: {string.Join(", ", outputCompilationDiagnostics)}");
-        }
+        _ = driver.RunGeneratorsAndUpdateCompilation(originalCompilation, out compilation, out diagnostics);
     }
 }
