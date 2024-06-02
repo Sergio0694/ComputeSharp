1248c9e796778c3f7e04dd480e827bcfbf1bf4d8
Sergio Pedri <sergio0694@live.com>
2024-06-03 01:13:01 +0200

    Initial prototype to gather property infos

--- src/ComputeSharp.D2D1.WinUI.SourceGenerators/CanvasEffectPropertyGenerator.Execute.cs
+++ src/ComputeSharp.D2D1.WinUI.SourceGenerators/CanvasEffectPropertyGenerator.Execute.cs
@@ -38,5 +38,36 @@ partial class CanvasEffectPropertyGenerator
             // The property must be in a type with a base type (as it must derive from CanvasEffect)
             return node.Parent?.IsTypeDeclarationWithOrPotentiallyWithBaseTypes<ClassDeclarationSyntax>() == true;
         }
+
+        /// <summary>
+        /// Checks whether the generated code has to directly reference the old property value.
+        /// </summary>
+        /// <param name="propertySymbol">The input <see cref="IPropertySymbol"/> instance to process.</param>
+        /// <returns>Whether the generated code needs direct access to the old property value.</returns>
+        public static bool IsOldPropertyValueDirectlyReferenced(IPropertySymbol propertySymbol)
+        {
+            // Check On<PROPERTY_NAME>Changing(<PROPERTY_TYPE> oldValue, <PROPERTY_TYPE> newValue) first
+            foreach (ISymbol symbol in propertySymbol.ContainingType.GetMembers($"On{propertySymbol.Name}Changing"))
+            {
+                // No need to be too specific as we're not expecting false positives (which also wouldn't really
+                // cause any problems anyway, just produce slightly worse codegen). Just checking the number of
+                // parameters is good enough, and keeps the code very simple and cheap to run.
+                if (symbol is IMethodSymbol { Parameters.Length: 2 })
+                {
+                    return true;
+                }
+            }
+
+            // Do the same for On<PROPERTY_NAME>Changed(<PROPERTY_TYPE> oldValue, <PROPERTY_TYPE> newValue)
+            foreach (ISymbol symbol in propertySymbol.ContainingType.GetMembers($"On{propertySymbol.Name}Changed"))
+            {
+                if (symbol is IMethodSymbol { Parameters.Length: 2 })
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
