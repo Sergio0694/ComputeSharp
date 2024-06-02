616766bc800202ae38de4c5b0e1bb81962562616
Sergio Pedri <sergio0694@live.com>
2024-06-03 01:12:41 +0200

    Add initial 'CanvasEffectPropertyInfo' type

---
+++ src/ComputeSharp.D2D1.WinUI.SourceGenerators/Models/CanvasEffectPropertyInfo.cs
@@ -0,0 +1,16 @@
+using ComputeSharp.SourceGeneration.Models;
+
+namespace ComputeSharp.D2D1.WinUI.SourceGenerators.Models;
+
+/// <summary>
+/// A model representing a generated canvas effect property.
+/// </summary>
+/// <param name="Hierarchy">The hierarchy info for the shader type.</param>
+/// <param name="TypeNameWithNullabilityAnnotations">The type name for the generated property, including nullability annotations.</param>
+/// <param name="PropertyName">The generated property name.</param>
+/// <param name="IsOldPropertyValueDirectlyReferenced">Whether the old property value is being directly referenced.</param>
+internal sealed record CanvasEffectPropertyInfo(
+    HierarchyInfo Hierarchy,
+    string PropertyName,
+    string TypeNameWithNullabilityAnnotations,
+    bool IsOldPropertyValueDirectlyReferenced);
