9f789d1bb3ffc8e84b872dc5a01c1483c1d8b807
Sergio Pedri <sergio0694@live.com>
2024-06-03 02:23:45 +0200

    Remove micro-optimization for old field values

--- src/ComputeSharp.D2D1.WinUI.SourceGenerators/Models/CanvasEffectPropertyInfo.cs
+++ src/ComputeSharp.D2D1.WinUI.SourceGenerators/Models/CanvasEffectPropertyInfo.cs
@@ -8,13 +8,11 @@ namespace ComputeSharp.D2D1.WinUI.SourceGenerators.Models;
 /// <param name="Hierarchy">The hierarchy info for the shader type.</param>
 /// <param name="TypeNameWithNullabilityAnnotations">The type name for the generated property, including nullability annotations.</param>
 /// <param name="PropertyName">The generated property name.</param>
-/// <param name="IsOldPropertyValueDirectlyReferenced">Whether the old property value is being directly referenced.</param>
 /// <param name="IsReferenceTypeOrUnconstraindTypeParameter">Indicates whether the property is of a reference type or an unconstrained type parameter.</param>
 /// <param name="InvalidationType">The invalidation type to request.</param>
 internal sealed record CanvasEffectPropertyInfo(
     HierarchyInfo Hierarchy,
     string PropertyName,
     string TypeNameWithNullabilityAnnotations,
-    bool IsOldPropertyValueDirectlyReferenced,
     bool IsReferenceTypeOrUnconstraindTypeParameter,
     CanvasEffectInvalidationType InvalidationType);
