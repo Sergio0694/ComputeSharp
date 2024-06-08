b71ea332a046fffe83c53078ed4be77e96f6014b
Sergio Pedri <sergio0694@live.com>
2024-06-07 18:51:55 -0700

    Remove shader linking heuristics in generator

--- src/ComputeSharp.D2D1.SourceGenerators/Diagnostics/Analyzers/InvalidD2D1PixelOptionsTrivialSamplingOnShaderTypeAnalyzer.cs
+++ src/ComputeSharp.D2D1.SourceGenerators/Diagnostics/Analyzers/InvalidD2D1PixelOptionsTrivialSamplingOnShaderTypeAnalyzer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Immutable;
 using ComputeSharp.SourceGeneration.Extensions;
 using Microsoft.CodeAnalysis;
@@ -54,7 +55,7 @@ public sealed class InvalidD2D1PixelOptionsTrivialSamplingOnShaderTypeAnalyzer :
                 }
 
                 // Emit a diagnostic if the compile options are not valid for the shader type
-                if (!D2DPixelShaderDescriptorGenerator.HlslBytecode.IsSimpleInputShader(typeSymbol, d2DInputSimpleAttributeSymbol, inputCount))
+                if (!IsSimpleInputShader(typeSymbol, d2DInputSimpleAttributeSymbol, inputCount))
                 {
                     context.ReportDiagnostic(Diagnostic.Create(
                         InvalidD2D1PixelOptionsTrivialSamplingOnShaderType,
@@ -63,5 +64,57 @@ public sealed class InvalidD2D1PixelOptionsTrivialSamplingOnShaderTypeAnalyzer :
                 }
             }, SymbolKind.NamedType);
         });
+    }
+
+    /// <summary>
+    /// Checks whether a given shader only has simple inputs.
+    /// </summary>
+    /// <param name="structDeclarationSymbol">The input <see cref="INamedTypeSymbol"/> instance to process.</param>
+    /// <param name="d2DInputSimpleSymbolAttributeSymbol">The symbol for the <c>[D2DInputSimple]</c> attribute.</param>
+    /// <param name="inputCount">The number of inputs for the shader.</param>
+    /// <returns>Whether the shader only has simple inputs.</returns>
+    private static bool IsSimpleInputShader(
+        INamedTypeSymbol structDeclarationSymbol,
+        INamedTypeSymbol d2DInputSimpleSymbolAttributeSymbol,
+        int inputCount)
+    {
+        // We cannot trust the input count to be valid at this point (it may be invalid and
+        // with diagnostic already emitted for it). So first, just clamp it in the right range.
+        inputCount = Math.Max(0, Math.Min(8, inputCount));
+
+        // If there are no inputs, the shader is as if only had simple inputs
+        if (inputCount == 0)
+        {
+            return true;
+        }
+
+        // Build a map of all simple inputs (unmarked inputs default to being complex).
+        // We can never have more than 8 inputs, and if there are it means the shader is
+        // not valid. Just ignore them, and the generator will emit a separate diagnostic.
+        Span<bool> simpleInputsMap = stackalloc bool[8];
+
+        // We first start with all inputs marked as complex (ie. not simple)
+        simpleInputsMap.Clear();
+
+        foreach (AttributeData attributeData in structDeclarationSymbol.GetAttributes())
+        {
+            // Only retrieve indices of simple inputs that are in range. If an input is out of
+            // range, the diagnostic for it will already be emitted by a previous generator step.
+            if (SymbolEqualityComparer.Default.Equals(attributeData.AttributeClass, d2DInputSimpleSymbolAttributeSymbol) &&
+                attributeData.ConstructorArguments is [{ Value: >= 0 and < 8 and int index }])
+            {
+                simpleInputsMap[index] = true;
+            }
+        }
+
+        bool isSimpleInputShader = true;
+
+        // Validate all inputs in our range (filtered by the allowed one)
+        foreach (bool isSimpleInput in simpleInputsMap[..inputCount])
+        {
+            isSimpleInputShader &= isSimpleInput;
+        }
+
+        return isSimpleInputShader;
     }
 }
