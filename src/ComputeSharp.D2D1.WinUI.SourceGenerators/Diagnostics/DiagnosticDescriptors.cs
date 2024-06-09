c50bf84c7219f894a39c254ac4c01be7196cb182
Sergio Pedri <sergio0694@live.com>
2024-06-09 09:40:10 -0700

    Add 'InvalidGeneratedCanvasEffectPropertyAccessorsAnalyzer'

--- src/ComputeSharp.D2D1.WinUI.SourceGenerators/Diagnostics/DiagnosticDescriptors.cs
+++ src/ComputeSharp.D2D1.WinUI.SourceGenerators/Diagnostics/DiagnosticDescriptors.cs
@@ -22,4 +22,20 @@ internal static class DiagnosticDescriptors
         isEnabledByDefault: true,
         description: "A property using [GeneratedCanvasEffectProperty] is in a type that does not derive from CanvasEffect ([GeneratedCanvasEffectProperty] must be used in effect types that extend CanvasEffect).",
         helpLinkUri: "https://github.com/Sergio0694/ComputeSharp");
+
+    /// <summary>
+    /// Gets a <see cref="DiagnosticDescriptor"/> for a CanvasEffect property with invalid accessors.
+    /// <para>
+    /// Format: <c>"The property "{0}" does not have a getter and a setter ([GeneratedCanvasEffectProperty] must be used on properties with get and set accessors)"</c>.
+    /// </para>
+    /// </summary>
+    public static readonly DiagnosticDescriptor InvalidGeneratedCanvasEffectPropertyAccessors = new(
+        id: "CMPSD2DWINUI0002",
+        title: "Invalid generated CanvasEffect property declaration",
+        messageFormat: """The property "{0}" does not have a getter and a setter ([GeneratedCanvasEffectProperty] must be used on properties with get and set accessors)""",
+        category: "ComputeSharp.D2D1.WinUI.Effects",
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true,
+        description: "A property using [GeneratedCanvasEffectProperty] does not have a getter and a setter ([GeneratedCanvasEffectProperty] must be used on properties with get and set accessors).",
+        helpLinkUri: "https://github.com/Sergio0694/ComputeSharp");
 }
